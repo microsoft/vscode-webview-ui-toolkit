d21d964f595c62c82bbd2dbcb8ac5a8e1819c8f9
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-02-19 17:55:24 -0800

    Create VSCode Design System file and add initial interface and defaults object

---
+++ src/vscode-design-system.ts
@@ -0,0 +1,11 @@
+/**
+ * Defines the properties in the FAST Design System
+ * @public
+ */
+export interface DesignSystem {}
+
+/**
+ * The default values for {@link DesignSystem}
+ * @public
+ */
+export const DesignSystemDefaults: DesignSystem = {};
