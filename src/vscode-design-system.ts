179a8d64a120db422821882a885e546333318318
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-02-22 11:57:01 -0800

    Add some design tokens to the vscode-design-system file

--- src/vscode-design-system.ts
+++ src/vscode-design-system.ts
@@ -2,10 +2,20 @@
  * Defines the properties in the FAST Design System
  * @public
  */
-export interface DesignSystem {}
+export interface DesignSystem {
+	accentBaseColor: string;
+	accentHoverColor: string;
+	cornerRadius: number;
+	/* Insert other design tokens */
+}
 
 /**
  * The default values for {@link DesignSystem}
  * @public
  */
-export const DesignSystemDefaults: DesignSystem = {};
+export const DesignSystemDefaults: DesignSystem = {
+	accentBaseColor: '#0e639c',
+	accentHoverColor: '#007acc',
+	cornerRadius: 0,
+	/* Insert the default values for other design tokens */
+};
