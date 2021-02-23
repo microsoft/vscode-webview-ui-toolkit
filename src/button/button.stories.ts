22f790eb1d0d8df2fb2e28f8ce131c15b0af00aa
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-02-23 15:42:22 -0800

    Add arg controls and new stories to button.stories.ts

--- src/button/button.stories.ts
+++ src/button/button.stories.ts
@@ -1,6 +1,7 @@
 import {VSCodeDesignSystemProvider} from '../design-system-provider/index';
-import ButtonTemplate from './fixtures/button.html';
+import {createButton, ButtonArgs} from './fixtures/createButton';
 import {VSCodeButton} from './index';
+import {action} from '@storybook/addon-actions';
 
 // Prevent tree-shaking
 VSCodeButton;
@@ -8,6 +9,26 @@ VSCodeDesignSystemProvider;
 
 export default {
 	title: 'Library/Button',
+	argTypes: {
+		label: {control: 'text'},
+		onClick: {action: 'onClick'},
+		disabled: {control: 'boolean'},
+	},
 };
 
-export const Button = (): string => ButtonTemplate;
+const Template = ({...args}: ButtonArgs) => {
+	return createButton({...args});
+};
+
+export const Default: any = Template.bind({});
+Default.args = {
+	label: 'Button',
+	onClick: action('button-clicked'),
+	disabled: false,
+};
+
+export const Disabled: any = Template.bind({});
+Disabled.args = {
+	...Default.args,
+	disabled: true,
+};
