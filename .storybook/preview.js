188258eed9108520e316f76123556382587abd01
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-02-23 14:38:01 -0800

    Configure global storybook decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,21 @@
 import {themes} from '@storybook/theming';
+import {addDecorator} from '@storybook/html';
+
+// Add global decorator that wraps all stories in the
+// <vscode-design-system-provider> custom element
+addDecorator(story => {
+	const storyContents = story();
+	return createDesignProviderWrapper(storyContents);
+});
+
+const createDesignProviderWrapper = story => {
+	const designProvider = document.createElement(
+		'vscode-design-system-provider'
+	);
+	designProvider.setAttribute('use-defaults', '');
+	designProvider.appendChild(story);
+	return designProvider;
+};
 
 export const parameters = {
 	actions: {argTypesRegex: '^on[A-Z].*'},
