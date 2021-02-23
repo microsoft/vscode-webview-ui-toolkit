945f3d329dd2987d4c6a4bfd6a82a292e64b54f2
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-02-22 17:08:20 -0800

    Add Storybook config parameters to preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,31 @@
+import {themes} from '@storybook/theming';
+
 export const parameters = {
 	actions: {argTypesRegex: '^on[A-Z].*'},
+	options: {
+		storySort: {
+			order: ['Docs', 'Library'],
+		},
+		isToolshown: true,
+	},
+	docs: {
+		theme: themes.light,
+	},
+	backgrounds: {
+		default: 'light',
+		values: [
+			{
+				name: 'light',
+				value: '#f9f9f9',
+			},
+			{
+				name: 'grey',
+				value: '#929396',
+			},
+			{
+				name: 'dark',
+				value: '#333',
+			},
+		],
+	},
 };
