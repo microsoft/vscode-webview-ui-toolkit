b65e0a8842696787dfed986fbd9c8861b8898fa3
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-02-22 17:28:03 -0800

    Update storybook main config file to start searching for storybook files in the /docs directory

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
 	stories: [
 		'../src/**/*.stories.mdx',
 		'../src/**/*.stories.@(js|jsx|ts|tsx)',
+		'../docs/**/*.stories.mdx',
 	],
 	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
