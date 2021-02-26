7b53becdb5921f5952d484581636189a6822140a
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-02-26 10:09:10 -0800

    Update Storybook babel config so components render when the Storybook static website is built

--- .storybook/babel.config.js
+++ .storybook/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	presets: ['@babel/preset-env'],
+	presets: ['@babel/preset-env', '@babel/preset-typescript'],
 	plugins: [
-		['@babel/plugin-proposal-decorators', {decoratorsBeforeExport: true}],
+		['@babel/plugin-proposal-decorators', {legacy: true}],
 		['@babel/plugin-proposal-class-properties', {loose: true}],
 	],
 };
