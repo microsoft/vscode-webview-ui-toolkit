b97607d90d02aa1ba58141e702fa8787d7ad75f8
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-02-26 15:57:16 -0800

    Remove style definitions and boilerplate comments

--- src/button/index.ts
+++ src/button/index.ts
@@ -1,25 +1,6 @@
-import {css, customElement} from '@microsoft/fast-element';
+import {customElement} from '@microsoft/fast-element';
 import {Button, ButtonTemplate as template} from '@microsoft/fast-foundation';
-
-const styles = css`
-	.control {
-		border-radius: calc(var(--corner-radius) * 1px);
-		color: white;
-		background-color: var(--accent-base-color);
-		border: none;
-		padding: 10px 10px;
-		box-sizing: border-box;
-		display: flex;
-		text-align: center;
-		justify-content: center;
-		align-items: center;
-		cursor: pointer;
-	}
-
-	.control:hover {
-		background-color: var(--accent-hover-color);
-	}
-`;
+import {ButtonStyles as styles} from './button.styles';
 
 @customElement({
 	name: 'vscode-button',
@@ -29,11 +10,4 @@ const styles = css`
 		delegatesFocus: true,
 	},
 })
-export class VSCodeButton extends Button {
-	/**
-	 * This new VSCode button should work right out of the box because it inherits all the
-	 * behaviors/functionality from the FAST Foundation Button class.
-	 *
-	 * With that said, we can still add new behaviors/functionality to this VSCode button if we want.
-	 */
-}
+export class VSCodeButton extends Button {}
