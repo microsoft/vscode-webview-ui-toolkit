69e589bb4602dbbf914b34f9fc9a1840166020a0
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-01 13:08:54 -0800

    Implement appearance attribute in VSCodeButton class

--- src/button/index.ts
+++ src/button/index.ts
@@ -1,7 +1,29 @@
-import {customElement} from '@microsoft/fast-element';
+import {attr, customElement} from '@microsoft/fast-element';
 import {Button, ButtonTemplate as template} from '@microsoft/fast-foundation';
 import {ButtonStyles as styles} from './button.styles';
 
+/**
+ * Types of button appearance.
+ * @public
+ */
+export type ButtonAppearance =
+	| 'install'
+	| 'link'
+	| 'primary'
+	| 'secondary'
+	| 'text';
+
+/**
+ * The VSCode Button Element. Extends {@link @microsoft/fast-foundation#Button},
+ * {@link @microsoft/fast-foundation#ButtonTemplate}
+ *
+ *
+ * @public
+ * @remarks
+ * HTML Element: \<vscode-button\>
+ *
+ * {@link https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/delegatesFocus | delegatesFocus}
+ */
 @customElement({
 	name: 'vscode-button',
 	template,
@@ -10,4 +32,20 @@ import {ButtonStyles as styles} from './button.styles';
 		delegatesFocus: true,
 	},
 })
-export class VSCodeButton extends Button {}
+export class VSCodeButton extends Button {
+	/**
+	 * The appearance the button should have.
+	 *
+	 * @public
+	 * @remarks
+	 * HTML Attribute: appearance
+	 */
+	@attr public appearance: ButtonAppearance;
+
+	public connectedCallback() {
+		super.connectedCallback();
+		if (!this.appearance) {
+			this.appearance = 'primary';
+		}
+	}
+}
