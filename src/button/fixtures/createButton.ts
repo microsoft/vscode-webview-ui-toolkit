923dcec3587a644ae805e9b0701cb4cf080e7a40
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-01 13:10:43 -0800

    Update createButton fixture function to accept an appearance argument

--- src/button/fixtures/createButton.ts
+++ src/button/fixtures/createButton.ts
@@ -1,29 +1,34 @@
 import {VSCodeButton} from '../index';
+import {focusObserver} from '../../utilities/focusObserver';
+import {ButtonAppearance} from '../index';
 
 export type ButtonArgs = {
 	label: string;
-	onClick: any;
+	appearance?: ButtonAppearance;
 	isDisabled: boolean;
 	isFocused: boolean;
+	onClick: any;
 };
 
 export function createButton({
 	label,
-	onClick,
+	appearance,
 	isDisabled,
 	isFocused,
+	onClick,
 }: ButtonArgs) {
 	const button = new VSCodeButton();
 
 	button.textContent = label;
 	button.addEventListener('click', onClick);
+	if (appearance) {
+		button.setAttribute('appearance', appearance.toLowerCase());
+	}
 	if (isDisabled) {
 		button.setAttribute('disabled', '');
 	}
 	if (isFocused) {
-		window.addEventListener('load', () => {
-			button.focus();
-		});
+		focusObserver(button);
 	}
 
 	return button;
