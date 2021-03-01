ed92d9865cdc7da6a1ca61d07605a5843f8df59a
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-01 13:04:24 -0800

    Update VSCode design system token names and add new design tokens for secondary colors and text link colors

--- src/vscode-design-system.ts
+++ src/vscode-design-system.ts
@@ -4,16 +4,33 @@
  */
 export interface DesignSystem {
 	/**
-	 * The accent colors.
+	 * The primary colors.
 	 */
-	accentBaseColor: string;
-	accentForegroundColor: string;
-	accentHoverColor: string;
+	primaryBackgroundColor: string;
+	primaryForegroundColor: string;
+	primaryHoverBackgroundColor: string;
+
+	/**
+	 * The secondary colors.
+	 */
+	secondaryBackgroundColor: string;
+	secondaryForegroundColor: string;
+	secondaryHoverBackgroundColor: string;
+
+	/**
+	 * Text link colors.
+	 */
+	textLinkForegroundColor: string;
 
 	/**
 	 * The corner radius applied to controls.
 	 */
 	cornerRadius: number;
+
+	/**
+	 * The opacity of a disabled control.
+	 */
+	disabledOpacity: number;
 
 	/**
 	 * The width of the standard outline applied to outline components in pixels.
@@ -24,11 +41,6 @@ export interface DesignSystem {
 	 * The width of the standard focus outline in pixels.
 	 */
 	focusOutlineWidth: number;
-
-	/**
-	 * The opacity of a disabled control.
-	 */
-	disabledOpacity: number;
 }
 
 /**
@@ -36,11 +48,38 @@ export interface DesignSystem {
  * @public
  */
 export const DesignSystemDefaults: DesignSystem = {
-	accentBaseColor: '#0e639c',
-	accentForegroundColor: '#ffffff',
-	accentHoverColor: '#1177bb',
+	/**
+	 * The primary color defaults.
+	 */
+	primaryBackgroundColor: '#0e639c',
+	primaryForegroundColor: '#ffffff',
+	primaryHoverBackgroundColor: '#1177bb',
+
+	/**
+	 * The secondary color defaults.
+	 */
+	secondaryBackgroundColor: '#3a3d41',
+	secondaryForegroundColor: '#ffffff',
+	secondaryHoverBackgroundColor: '#45494e',
+
+	/**
+	 * Text link defaults.
+	 */
+	textLinkForegroundColor: "#3794ff",
+
+	/**
+	 * The corner radius default.
+	 */
 	cornerRadius: 0,
+
+	/**
+	 * The corner radius default.
+	 */
 	disabledOpacity: 0.4,
+
+	/**
+	 * The outline defaults.
+	 */
 	outlineWidth: 1,
 	focusOutlineWidth: 1,
 };
