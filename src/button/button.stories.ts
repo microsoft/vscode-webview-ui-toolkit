37e726556c946a173417fdadf9855d5fb5e05d5b
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-01 13:11:30 -0800

    Add Storybook stories for secondary, install, text, and link buttons

--- src/button/button.stories.ts
+++ src/button/button.stories.ts
@@ -11,9 +11,15 @@ export default {
 	title: 'Library/Button',
 	argTypes: {
 		label: {control: 'text'},
-		onClick: {action: 'onClick'},
+		appearance: {
+			control: {
+				type: 'select',
+				options: ['Primary', 'Secondary', 'Install', 'Text', 'Link'],
+			},
+		},
 		isDisabled: {control: 'boolean'},
 		isFocused: {control: 'boolean'},
+		onClick: {action: 'clicked'},
 	},
 };
 
@@ -24,19 +30,92 @@ const Template = ({...args}: ButtonArgs) => {
 export const Primary: any = Template.bind({});
 Primary.args = {
 	label: 'Button',
-	onClick: action('button-clicked'),
+	appearance: 'Primary',
 	isDisabled: false,
 	isFocused: false,
+	onClick: action('button-clicked'),
 };
 
-export const Disabled: any = Template.bind({});
-Disabled.args = {
+export const PrimaryDisabled: any = Template.bind({});
+PrimaryDisabled.args = {
 	...Primary.args,
 	isDisabled: true,
 };
 
-export const Focused: any = Template.bind({});
-Focused.args = {
+export const PrimaryFocused: any = Template.bind({});
+PrimaryFocused.args = {
 	...Primary.args,
+	isFocused: true,
+};
+
+export const Secondary: any = Template.bind({});
+Secondary.args = {
+	...Primary.args,
+	appearance: 'Secondary',
+};
+
+export const SecondaryDisabled: any = Template.bind({});
+SecondaryDisabled.args = {
+	...Secondary.args,
+	isDisabled: true,
+};
+
+export const SecondaryFocused: any = Template.bind({});
+SecondaryFocused.args = {
+	...Secondary.args,
+	isFocused: true,
+};
+
+export const Install: any = Template.bind({});
+Install.args = {
+	...Primary.args,
+	appearance: 'Install',
+};
+
+export const InstallDisabled: any = Template.bind({});
+InstallDisabled.args = {
+	...Install.args,
+	isDisabled: true,
+};
+
+export const InstallFocused: any = Template.bind({});
+InstallFocused.args = {
+	...Install.args,
+	isFocused: true,
+};
+
+export const Text: any = Template.bind({});
+Text.args = {
+	...Primary.args,
+	appearance: 'Text',
+};
+
+export const TextDisabled: any = Template.bind({});
+TextDisabled.args = {
+	...Text.args,
+	isDisabled: true,
+};
+
+export const TextFocused: any = Template.bind({});
+TextFocused.args = {
+	...Text.args,
+	isFocused: true,
+};
+
+export const Link: any = Template.bind({});
+Link.args = {
+	...Primary.args,
+	appearance: 'Link',
+};
+
+export const LinkDisabled: any = Template.bind({});
+LinkDisabled.args = {
+	...Link.args,
+	isDisabled: true,
+};
+
+export const LinkFocused: any = Template.bind({});
+LinkFocused.args = {
+	...Link.args,
 	isFocused: true,
 };
