4613d824f9ea05c337ba592711742674132d543e
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-01 16:47:46 -0800

    Create VSCode checkbox component class

---
+++ src/checkbox/index.ts
@@ -0,0 +1,25 @@
+import {customElement} from '@microsoft/fast-element';
+import {
+	Checkbox,
+	CheckboxTemplate as template,
+} from '@microsoft/fast-foundation';
+import {CheckboxStyles as styles} from './checkbox.styles';
+
+/**
+ * The VSCode Checkbox Element. Extends {@link @microsoft/fast-foundation#Checkbox},
+ * {@link @microsoft/fast-foundation#CheckboxTemplate}
+ *
+ *
+ * @public
+ * @remarks
+ * HTML Element: \<vscode-checkbox\>
+ */
+@customElement({
+	name: 'vscode-checkbox',
+	template,
+	styles,
+	shadowOptions: {
+		delegatesFocus: true,
+	},
+})
+export class VSCodeCheckbox extends Checkbox {}
