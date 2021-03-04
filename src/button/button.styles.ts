7d65a27beadcbda6c910bb86915e327221d97c9a
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-03 16:26:44 -0800

    Updated VSCode Button styles to consume updated/new design tokens

--- src/button/button.styles.ts
+++ src/button/button.styles.ts
@@ -1,5 +1,9 @@
 import {css} from '@microsoft/fast-element';
-import {disabledCursor, display} from '@microsoft/fast-foundation';
+import {
+	disabledCursor,
+	display,
+	focusVisible,
+} from '@microsoft/fast-foundation';
 
 /**
  * @internal
@@ -7,8 +11,8 @@ import {disabledCursor, display} from '@microsoft/fast-foundation';
 const BaseButtonStyles = css`
 	${display('inline-flex')} :host {
 		outline: none;
-		background: var(--primary-background-color);
-		color: var(--primary-foreground-color);
+		background: var(--button-primary-background-color);
+		color: var(--button-primary-foreground-color);
 		border-radius: calc(var(--corner-radius) * 1px);
 		fill: currentcolor;
 		cursor: pointer;
@@ -25,7 +29,7 @@ const BaseButtonStyles = css`
 		white-space: nowrap;
 		outline: none;
 		text-decoration: none;
-		border: calc(var(--outline-width) * 1px) solid transparent;
+		border: calc(var(--border-width) * 1px) solid transparent;
 		color: inherit;
 		border-radius: inherit;
 		fill: inherit;
@@ -33,22 +37,22 @@ const BaseButtonStyles = css`
 		font-family: inherit;
 	}
 	:host(:hover) {
-		background: var(--primary-hover-background-color);
+		background: var(--button-primary-hover-background-color);
 	}
 	:host(:active) {
-		background: var(--primary-background-color);
+		background: var(--button-primary-background-color);
 	}
-	.control:focus {
-		outline: calc(var(--outline-width) * 1px) solid
-			var(--primary-hover-background-color);
-		outline-offset: calc(var(--outline-width) * 1px);
+	.control: ${focusVisible} {
+		outline: calc(var(--focus-border-width) * 1px) solid
+			var(--button-primary-hover-background-color);
+		outline-offset: calc(var(--focus-border-width) * 1px);
 	}
 	.control::-moz-focus-inner {
 		border: 0;
 	}
 	:host([disabled]) {
 		opacity: var(--disabled-opacity);
-		background: var(--primary-background-color);
+		background: var(--button-primary-background-color);
 		cursor: ${disabledCursor};
 	}
 	.start,
@@ -60,10 +64,7 @@ const BaseButtonStyles = css`
 		line-height: 0;
 	}
 	::slotted(svg) {
-		${
-			/* Glyph size and margin-left is temporary -
-            replace when adaptive typography is figured out */ ''
-		} width: 16px;
+		width: 16px;
 		height: 16px;
 	}
 	.start {
@@ -79,24 +80,25 @@ const BaseButtonStyles = css`
  */
 const PrimaryButtonStyles = css`
 	:host([appearance='primary']) {
-		background: var(--primary-background-color);
-		color: var(--primary-foreground-color);
+		background: var(--button-primary-background-color);
+		color: var(--button-primary-foreground-color);
 	}
 	:host([appearance='primary']:hover) {
-		background: var(--primary-hover-background-color);
+		background: var(--button-primary-hover-background-color);
 	}
 	:host([appearance='primary']) .control {
 		padding: 6px 11px;
 	}
 	:host([appearance='primary']:active) .control:active {
-		background: var(--primary-background-color);
+		background: var(--button-primary-background-color);
 	}
-	:host([appearance='primary']) .control:focus {
-		outline: calc(var(--outline-width) * 1px) solid
-			var(--primary-hover-background-color);
+	:host([appearance='primary']) .control:${focusVisible} {
+		outline: calc(var(--focus-border-width) * 1px) solid
+			var(--button-primary-hover-background-color);
+		outline-offset: calc(var(--focus-border-width) * 1px);
 	}
 	:host([appearance='primary'][disabled]) {
-		background: var(--primary-background-color);
+		background: var(--button-primary-background-color);
 	}
 `;
 
@@ -105,24 +107,25 @@ const PrimaryButtonStyles = css`
  */
 const SecondaryButtonStyles = css`
 	:host([appearance='secondary']) {
-		background: var(--secondary-background-color);
-		color: var(--secondary-foreground-color);
+		background: var(--button-secondary-background-color);
+		color: var(--button-secondary-foreground-color);
 	}
 	:host([appearance='secondary']:hover) {
-		background: var(--secondary-hover-background-color);
+		background: var(--button-secondary-hover-background-color);
 	}
 	:host([appearance='secondary']) .control {
 		padding: 6px 11px;
 	}
 	:host([appearance='secondary']:active) .control:active {
-		background: var(--secondary-background-color);
+		background: var(--button-secondary-background-color);
 	}
-	:host([appearance='secondary']) .control:focus {
-		outline: calc(var(--outline-width) * 1px) solid
-			var(--primary-hover-background-color);
+	:host([appearance='secondary']) .control:${focusVisible} {
+		outline: calc(var(--focus-border-width) * 1px) solid
+			var(--button-primary-hover-background-color);
+		outline-offset: calc(var(--focus-border-width) * 1px);
 	}
 	:host([appearance='secondary'][disabled]) {
-		background: var(--secondary-background-color);
+		background: var(--button-secondary-background-color);
 	}
 `;
 
@@ -131,24 +134,25 @@ const SecondaryButtonStyles = css`
  */
 const InstallButtonStyles = css`
 	:host([appearance='install']) {
-		background: var(--primary-background-color);
-		color: var(--primary-foreground-color);
+		background: var(--button-primary-background-color);
+		color: var(--button-primary-foreground-color);
 	}
 	:host([appearance='install']:hover) {
-		background: var(--primary-hover-background-color);
+		background: var(--button-primary-hover-background-color);
 	}
 	:host([appearance='install']) .control {
 		padding: 2px 6px;
 	}
 	:host([appearance='install']:active) .control:active {
-		background: var(--primary-background-color);
+		background: var(--button-primary-background-color);
 	}
-	:host([appearance='install']) .control:focus {
-		outline: calc(var(--outline-width) * 1px) solid
-			var(--primary-hover-background-color);
+	:host([appearance='install']) .control:${focusVisible} {
+		outline: calc(var(--focus-border-width) * 1px) solid
+			var(--button-primary-hover-background-color);
+		outline-offset: calc(var(--focus-border-width) * 1px);
 	}
 	:host([appearance='install'][disabled]) {
-		background: var(--primary-background-color);
+		background: var(--button-primary-background-color);
 	}
 `;
 
@@ -158,23 +162,24 @@ const InstallButtonStyles = css`
 const TextButtonStyles = css`
 	:host([appearance='text']) {
 		background: transparent;
-		color: var(--secondary-foreground-color);
+		color: var(--button-secondary-foreground-color);
 	}
 	:host([appearance='text']:hover) {
-		background: var(--secondary-hover-background-color);
+		background: var(--button-secondary-hover-background-color);
 	}
 	:host([appearance='text']) .control {
 		padding: 6px 11px;
 	}
 	:host([appearance='text']:active) .control:active {
-		background: var(--secondary-background-color);
+		background: var(--button-secondary-background-color);
 	}
-	:host([appearance='text']) .control:focus {
-		outline: calc(var(--outline-width) * 1px) solid
-			var(--primary-hover-background-color);
+	:host([appearance='text']) .control:${focusVisible} {
+		outline: calc(var(--focus-border-width) * 1px) solid
+			var(--button-primary-hover-background-color);
+		outline-offset: calc(var(--focus-border-width) * 1px);
 	}
 	:host([appearance='text'][disabled]) {
-		background: var(--secondary-background-color);
+		background: var(--button-secondary-background-color);
 	}
 `;
 
@@ -195,9 +200,10 @@ const LinkButtonStyles = css`
 	:host([appearance='link']:active) .control:active {
 		background: transparent;
 	}
-	:host([appearance='link']) .control:focus {
-		outline: calc(var(--outline-width) * 1px) solid
+	:host([appearance='link']) .control:${focusVisible} {
+		outline: calc(var(--focus-border-width) * 1px) solid
 			var(--text-link-foreground-color);
+		outline-offset: calc(var(--focus-border-width) * 1px);
 	}
 	:host([appearance='link'][disabled]) {
 		text-decoration: none;
