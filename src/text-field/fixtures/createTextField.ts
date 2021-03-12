84630ebc941e5ce3b09e2591cb6aa0f101a0a1d7
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-12 14:33:01 -0800

    Implement createTextField fixtures function

---
+++ src/text-field/fixtures/createTextField.ts
@@ -0,0 +1,53 @@
+import {VSCodeTextField} from '../index';
+
+export type TextFieldArgs = {
+	label?: string;
+	placeholder?: string;
+	minLength?: number;
+	maxLength?: number;
+	isRequired?: boolean;
+	isReadOnly?: boolean;
+	isDisabled?: boolean;
+	isAutoFocused?: boolean;
+};
+
+export function createTextField({
+	label,
+	placeholder,
+	minLength,
+	maxLength,
+	isRequired,
+	isReadOnly,
+	isDisabled,
+	isAutoFocused,
+}: TextFieldArgs) {
+	const textField = new VSCodeTextField();
+
+	if (label) {
+		textField.textContent = label;
+	}
+	if (placeholder) {
+		textField.setAttribute('placeholder', placeholder);
+	}
+	if (minLength) {
+		textField.setAttribute('minlength', minLength.toString());
+	}
+	if (maxLength) {
+		textField.setAttribute('maxlength', maxLength.toString());
+	}
+	if (isRequired) {
+		textField.setAttribute('required', '');
+	}
+	if (isReadOnly) {
+		textField.setAttribute('readonly', '');
+		textField.setAttribute('value', 'Read Only Text');
+	}
+	if (isDisabled) {
+		textField.setAttribute('disabled', '');
+	}
+	if (isAutoFocused) {
+		textField.setAttribute('autofocus', '');
+	}
+
+	return textField;
+}
