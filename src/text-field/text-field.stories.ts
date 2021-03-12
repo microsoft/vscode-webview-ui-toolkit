6981aabf8942c1bff8329c1e73ae3dd66c9bc59b
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-12 14:33:21 -0800

    Implement some VSCode Text Field component Storybook stories

---
+++ src/text-field/text-field.stories.ts
@@ -0,0 +1,41 @@
+import {VSCodeDesignSystemProvider} from '../design-system-provider/index';
+import {createTextField, TextFieldArgs} from './fixtures/createTextField';
+import {VSCodeTextField} from './index';
+
+// Prevent tree-shaking
+VSCodeTextField;
+VSCodeDesignSystemProvider;
+
+export default {
+	title: 'Library/Text Field',
+	argTypes: {
+		label: {control: 'text'},
+		placeholder: {control: 'text'},
+		minLength: {control: 'number'},
+		maxLength: {control: 'number'},
+		isRequired: {control: 'boolean'},
+		isReadOnly: {control: 'boolean'},
+		isDisabled: {control: 'boolean'},
+		isAutoFocused: {control: 'boolean'},
+	},
+};
+
+const Template = ({...args}: TextFieldArgs) => {
+	return createTextField({...args});
+};
+
+export const Default: any = Template.bind({});
+Default.args = {
+	label: 'Text Field Label',
+	placeholder: 'Placeholder Text',
+	isRequired: false,
+	isReadOnly: false,
+	isDisabled: false,
+	isAutoFocused: false,
+};
+
+export const NoLabel: any = Template.bind({});
+NoLabel.args = {
+	...Default.args,
+	label: '',
+};
