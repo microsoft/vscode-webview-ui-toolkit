0569c6befba21d2e1219938f7464c31c822c9f86
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-12 11:27:35 -0800

    Implement VSCode Link component class

---
+++ src/link/index.ts
@@ -0,0 +1,83 @@
+import {attr, customElement, ElementStyles} from '@microsoft/fast-element';
+import {Anchor, AnchorTemplate as template} from '@microsoft/fast-foundation';
+import {ButtonAppearance} from '../button';
+import {LinkStyles as styles} from './link.styles';
+
+/**
+ * Types of link appearance.
+ * @public
+ */
+export type LinkAppearance = ButtonAppearance | 'hypertext';
+
+/**
+ * The VSCode Link element. Extends
+ * {@link https://www.fast.design/docs/api/fast-foundation.anchor/ Anchor} and
+ * {@link https://www.fast.design/docs/api/fast-foundation.anchortemplate/ AnchorTemplate}.
+ *
+ * @public
+ * @remarks
+ * HTML Element: `<vscode-link>`
+ *
+ * Shadow Option: {@link https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/delegatesFocus delegatesFocus}
+ */
+@customElement({
+	name: 'vscode-link',
+	template,
+	styles,
+	shadowOptions: {
+		delegatesFocus: true,
+	},
+})
+export class VSCodeLink extends Anchor {
+	/**
+	 * The appearance the link should have.
+	 *
+	 * @public
+	 * @remarks
+	 * HTML Attribute: appearance
+	 */
+	@attr
+	public appearance: LinkAppearance;
+	public appearanceChanged(
+		oldValue: LinkAppearance,
+		newValue: LinkAppearance
+	): void {
+		if (oldValue !== newValue) {
+			this.classList.add(newValue);
+			this.classList.remove(oldValue);
+		}
+	}
+
+	public connectedCallback() {
+		super.connectedCallback();
+
+		if (!this.appearance) {
+			this.appearance = 'primary';
+		}
+	}
+
+	/**
+	 * Applies 'icon-only' class when there is only an SVG in the default slot
+	 *
+	 * @internal
+	 */
+	public defaultSlottedContentChanged(oldValue: any, newValue: any): void {
+		const slottedElements = this.defaultSlottedContent.filter(
+			x => x.nodeType === Node.ELEMENT_NODE
+		);
+		if (
+			slottedElements.length === 1 &&
+			slottedElements[0] instanceof SVGElement
+		) {
+			this.control.classList.add('icon-only');
+		} else {
+			this.control.classList.remove('icon-only');
+		}
+	}
+}
+
+/**
+ * Styles for Link
+ * @public
+ */
+export const LinkStyles: ElementStyles = styles;
