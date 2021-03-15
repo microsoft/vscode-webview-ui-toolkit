31a4bb97e8d668352bdf80465abf951e74481a36
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-15 14:36:02 -0700

    Export VSCode Data Grid component from src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './breadcrumb/index';
 export * from './breadcrumb-item/index';
 export * from './button/index';
 export * from './checkbox/index';
+export * from './data-grid/index';
 export * from './design-system-provider/index';
 export * from './link/index';
 export * from './option/index';
