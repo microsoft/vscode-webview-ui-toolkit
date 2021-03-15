ad8ac981eeedeea1c65be680f3d24a452bf4fc0e
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-15 15:26:59 -0700

    Add gridData arg control to Storybook stories

--- src/data-grid/data-grid.stories.ts
+++ src/data-grid/data-grid.stories.ts
@@ -9,7 +9,7 @@ VSCodeDesignSystemProvider;
 export default {
 	title: 'Library/Data Grid',
 	argTypes: {
-		isDisabled: {control: 'boolean'},
+		gridData: {control: 'object'},
 		isFocused: {control: 'boolean'},
 	},
 };
@@ -20,6 +20,32 @@ const Template = ({...args}: DataGridArgs) => {
 
 export const Default: any = Template.bind({});
 Default.args = {
-	isDisabled: false,
+	gridData: [
+		{
+			'Name': 'Container 1',
+			'Last Edited': '01/15/21',
+			'Description': 'This is a really great container',
+			'Source': 'Docker',
+		},
+		{
+			'Name': 'Container 2',
+			'Last Edited': '03/12/21',
+			'Description': 'This is a nice container too',
+			'Source': 'Docker',
+		},
+		{
+			'Name': 'Container 3',
+			'Last Edited': '05/04/21',
+			'Description':
+				'This is an incredibly long description that just goes on forever. Wow... still going.',
+			'Source': 'Docker',
+		},
+		{
+			'Name': 'Container 4',
+			'Last Edited': '06/22/21',
+			'Description': 'I use this container for important things',
+			'Source': 'Docker',
+		},
+	],
 	isFocused: false,
 };
