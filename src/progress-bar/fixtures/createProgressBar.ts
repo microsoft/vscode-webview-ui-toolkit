6899c249180cb3228fc8d9593085c3039b8c9646
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-18 16:40:41 -0700

    Implement createProgressBar fixtures function

---
+++ src/progress-bar/fixtures/createProgressBar.ts
@@ -0,0 +1,32 @@
+import {VSCodeProgressBar} from '../index';
+
+export type ProgressBarArgs = {
+	value: number;
+	min: number;
+	max: number;
+	isPaused: boolean;
+};
+
+export function createProgressBar({
+	value,
+	min,
+	max,
+	isPaused,
+}: ProgressBarArgs) {
+	const progressBar = new VSCodeProgressBar();
+
+	if (value) {
+		progressBar.setAttribute('value', value.toString());
+	}
+	if (min) {
+		progressBar.setAttribute('min', min.toString());
+	}
+	if (max) {
+		progressBar.setAttribute('max', max.toString());
+	}
+	if (isPaused) {
+		progressBar.setAttribute('paused', isPaused.toString());
+	}
+
+	return progressBar;
+}
