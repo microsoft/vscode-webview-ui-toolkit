39fa47891a14ab0b6293a84d3b6db3abde54761b
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-19 12:40:52 -0700

    Implement VSCode Divider component class

---
+++ src/divider/index.ts
@@ -0,0 +1,25 @@
+import {customElement, ElementStyles} from '@microsoft/fast-element';
+import {Divider, DividerTemplate as template} from '@microsoft/fast-foundation';
+import {DividerStyles as styles} from './divider.styles';
+
+/**
+ * The VSCode Divider element. Extends
+ * {@link https://www.fast.design/docs/api/fast-foundation.divider/ Divider} and
+ * {@link https://www.fast.design/docs/api/fast-foundation.dividertemplate/ DividerTemplate}.
+ *
+ * @public
+ * @remarks
+ * HTML Element: `<vscode-divider>`
+ */
+@customElement({
+	name: 'vscode-divider',
+	template,
+	styles,
+})
+export class VSCodeDivider extends Divider {}
+
+/**
+ * Styles for Divider
+ * @public
+ */
+export const DividerStyles: ElementStyles = styles;
