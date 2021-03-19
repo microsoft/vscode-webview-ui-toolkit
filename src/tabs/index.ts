b151a53fb1c569d59d5d34ecc601c6e54641c897
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-19 15:19:49 -0700

    Implement VSCode Tabs component class

---
+++ src/tabs/index.ts
@@ -0,0 +1,28 @@
+import {customElement, ElementStyles} from '@microsoft/fast-element';
+import {Tabs, TabsTemplate as template} from '@microsoft/fast-foundation';
+import {TabsStyles as styles} from './tabs.styles';
+
+/**
+ * The VSCode Tabs element. Extends
+ * {@link https://www.fast.design/docs/api/fast-foundation.tabs/ Tabs} and
+ * {@link https://www.fast.design/docs/api/fast-foundation.tabstemplate/ TabsTemplate}.
+ *
+ * @public
+ * @remarks
+ * HTML Element: `<vscode-tabs>`
+ */
+@customElement({
+	name: 'vscode-tabs',
+	template,
+	styles,
+})
+export class VSCodeTabs extends Tabs {}
+
+export * from '../tab/index';
+export * from '../tab-panel/index';
+
+/**
+ * Styles for Tabs
+ * @public
+ */
+export const TabsStyles: ElementStyles = styles;
