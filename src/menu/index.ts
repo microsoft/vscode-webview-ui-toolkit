3902bbad7a29c402691f8aa413b3de533c09692e
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-19 12:14:18 -0700

    Implement VSCode Menu component class

---
+++ src/menu/index.ts
@@ -0,0 +1,25 @@
+import {customElement, ElementStyles} from '@microsoft/fast-element';
+import {Menu, MenuTemplate as template} from '@microsoft/fast-foundation';
+import {MenuStyles as styles} from './menu.styles';
+
+/**
+ * The VSCode Menu element. Extends
+ * {@link https://www.fast.design/docs/api/fast-foundation.menu/ Menu} and
+ * {@link https://www.fast.design/docs/api/fast-foundation.menutemplate/ MenuTemplate}.
+ *
+ * @public
+ * @remarks
+ * HTML Element: `<vscode-menu>`
+ */
+@customElement({
+	name: 'vscode-menu',
+	template,
+	styles,
+})
+export class VSCodeMenu extends Menu {}
+
+/**
+ * Styles for Menu
+ * @public
+ */
+export const MenuStyles: ElementStyles = styles;
