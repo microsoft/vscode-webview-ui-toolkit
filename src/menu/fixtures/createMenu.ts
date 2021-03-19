cd834d274b0a750bfb105feafd3e92426d0a1b4d
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-19 12:53:28 -0700

    Add VSCode Divider to VSCode Menu stories and add styling rules for a slotted VSCode Divider in the VSCode Menu

--- src/menu/fixtures/createMenu.ts
+++ src/menu/fixtures/createMenu.ts
@@ -1,21 +1,30 @@
 import {VSCodeMenu} from '../index';
 import {VSCodeMenuItem} from '../../menu-item/index';
+import {VSCodeDivider} from '../../divider/index';
 
 export type MenuArgs = {
 	numberOfChildren: number;
+	withDividers: boolean;
 };
 
-export function createMenu({numberOfChildren}: MenuArgs) {
-	return createMenuWithNChildren(numberOfChildren);
+export function createMenu({numberOfChildren, withDividers}: MenuArgs) {
+	return createMenuWithNChildren(numberOfChildren, withDividers);
 }
 
-function createMenuWithNChildren(numberOfChildren: number) {
+function createMenuWithNChildren(
+	numberOfChildren: number,
+	withDividers: boolean
+) {
 	const menu = new VSCodeMenu();
 
 	for (let i = 0; i < numberOfChildren; i++) {
 		const menuItem = new VSCodeMenuItem();
 		menuItem.textContent = `Menu Item #${i + 1}`;
 		menu.appendChild(menuItem);
+		if (i % 2 === 0 && withDividers) {
+			const divider = new VSCodeDivider();
+			menu.appendChild(divider);
+		}
 	}
 
 	return menu;
