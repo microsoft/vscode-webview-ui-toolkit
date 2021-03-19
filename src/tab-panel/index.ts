27dd6d9fe92183f9a739208c7b73f12dac7ba43c
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-19 15:19:19 -0700

    Implement VSCode Tab Panel component class

---
+++ src/tab-panel/index.ts
@@ -0,0 +1,28 @@
+import {customElement, ElementStyles} from '@microsoft/fast-element';
+import {
+	TabPanel,
+	TabPanelTemplate as template,
+} from '@microsoft/fast-foundation';
+import {TabPanelStyles as styles} from './tab-panel.styles';
+
+/**
+ * The VSCode Tab Panel element. Extends
+ * {@link https://www.fast.design/docs/api/fast-foundation.tabpanel/ TabPanel} and
+ * {@link https://www.fast.design/docs/api/fast-foundation.tabpaneltemplate/ TabPanelTemplate}.
+ *
+ * @public
+ * @remarks
+ * HTML Element: `<vscode-tab-panel>`
+ */
+@customElement({
+	name: 'vscode-tab-panel',
+	template,
+	styles,
+})
+export class VSCodeTabPanel extends TabPanel {}
+
+/**
+ * Styles for TabPanel
+ * @public
+ */
+export const TabPanelStyles: ElementStyles = styles;
