9cf535ce8cefef54e1b7e139309823dc156a88d3
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-19 11:37:32 -0700

    Implement createMenuItem fixtures function

---
+++ src/menu-item/fixtures/createMenuItem.ts
@@ -0,0 +1,24 @@
+import {VSCodeMenuItem} from '../index';
+import {focusObserver} from '../../utilities/storybook/index';
+
+export type MenuItemArgs = {
+	label?: string;
+	isDisabled: boolean;
+	isFocused: boolean;
+};
+
+export function createMenuItem({label, isDisabled, isFocused}: MenuItemArgs) {
+	const menuItem = new VSCodeMenuItem();
+
+	if (label) {
+		menuItem.textContent = label;
+	}
+	if (isDisabled) {
+		menuItem.setAttribute('disabled', '');
+	}
+	if (isFocused) {
+		focusObserver(menuItem);
+	}
+
+	return menuItem;
+}
