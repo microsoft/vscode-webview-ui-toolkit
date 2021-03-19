b540c9536fd775043a4781fed1fe2551c56c2517
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-18 17:13:03 -0700

    Implement VSCode Progress Ring component class

---
+++ src/progress-ring/index.ts
@@ -0,0 +1,28 @@
+import {customElement, ElementStyles} from '@microsoft/fast-element';
+import {
+	BaseProgress,
+	ProgressRingTemplate as template,
+} from '@microsoft/fast-foundation';
+import {ProgressRingStyles as styles} from './progress-ring.styles';
+
+/**
+ * The VSCode Progress Ring element. Extends
+ * {@link https://www.fast.design/docs/api/fast-foundation.baseprogress/ BaseProgress} and
+ * {@link https://www.fast.design/docs/api/fast-foundation.progressringtemplate/ ProgressRingTemplate}.
+ *
+ * @public
+ * @remarks
+ * HTML Element: `<vscode-progress-ring>`
+ */
+@customElement({
+	name: 'vscode-progress-ring',
+	template,
+	styles,
+})
+export class VSCodeProgressRing extends BaseProgress {}
+
+/**
+ * Styles for ProgressRing
+ * @public
+ */
+export const ProgressRingStyles: ElementStyles = styles;
