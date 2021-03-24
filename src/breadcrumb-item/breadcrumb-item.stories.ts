5b68f70b6f6663188dfcd3eb0326b028d95b4cc8
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-24 14:25:27 -0700

    Add Storybook controls for including a custom element, start icon, separator icon, and end icon to the VSCode Breadcrumb Item

--- src/breadcrumb-item/breadcrumb-item.stories.ts
+++ src/breadcrumb-item/breadcrumb-item.stories.ts
@@ -14,6 +14,10 @@ export default {
 	argTypes: {
 		label: {control: 'text'},
 		href: {control: 'text'},
+		customElement: {control: 'boolean'},
+		startIcon: {control: 'boolean'},
+		separatorIcon: {control: 'boolean'},
+		endIcon: {control: 'boolean'},
 		isFocused: {control: 'boolean'},
 	},
 	parameters: {
@@ -30,13 +34,88 @@ const Template = ({...args}: BreadcrumbItemArgs) => {
 export const Default: any = Template.bind({});
 Default.args = {
 	label: 'src',
-	href: '#',
+	href: '',
+	customElement: false,
+	startIcon: false,
+	separatorIcon: false,
+	endIcon: false,
 	isFocused: false,
 };
 Default.parameters = {
 	docs: {
 		source: {
+			code: `<vscode-breadcrumb-item>src</vscode-breadcrumb-item>`,
+		},
+	},
+};
+
+export const WithLink: any = Template.bind({});
+WithLink.args = {
+	...Default.args,
+	href: '#',
+};
+WithLink.parameters = {
+	docs: {
+		source: {
 			code: `<vscode-breadcrumb-item href="#">src</vscode-breadcrumb-item>`,
+		},
+	},
+};
+
+export const WithCustomElement: any = Template.bind({});
+WithCustomElement.args = {
+	...Default.args,
+	href: '#',
+	customElement: true,
+};
+WithCustomElement.parameters = {
+	docs: {
+		source: {
+			code: `<vscode-breadcrumb-item href="#">\n\t<vscode-button>Button Element</vscode-button>\n</vscode-breadcrumb-item>`,
+		},
+	},
+};
+
+export const WithCustomSeparator: any = Template.bind({});
+WithCustomSeparator.args = {
+	...Default.args,
+	href: '#',
+	separatorIcon: true,
+};
+WithCustomSeparator.parameters = {
+	docs: {
+		source: {
+			code: `<!-- Context: Import VSCode Codicon Library -->\n\n<vscode-breadcrumb-item href="#">\n\tsrc\n\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n</vscode-breadcrumb-item>`,
+		},
+	},
+};
+
+export const WithStartIcon: any = Template.bind({});
+WithStartIcon.args = {
+	...Default.args,
+	href: '#',
+	startIcon: true,
+	separatorIcon: true,
+};
+WithStartIcon.parameters = {
+	docs: {
+		source: {
+			code: `<!-- Context: Import VSCode Codicon Library -->\n\n<vscode-breadcrumb-item href="#">\n\tsrc\n\t<svg slot="start" class="codicon codicon-symbol-method"></svg>\n\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n</vscode-breadcrumb-item>`,
+		},
+	},
+};
+
+export const WithEndIcon: any = Template.bind({});
+WithEndIcon.args = {
+	...Default.args,
+	href: '#',
+	separatorIcon: true,
+	endIcon: true,
+};
+WithEndIcon.parameters = {
+	docs: {
+		source: {
+			code: `<!-- Context: Import VSCode Codicon Library -->\n\n<vscode-breadcrumb-item href="#">\n\tsrc\n\t<svg slot="end" class="codicon codicon-symbol-method"></svg>\n\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n</vscode-breadcrumb-item>`,
 		},
 	},
 };
