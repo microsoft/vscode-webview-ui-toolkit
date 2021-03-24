1f92aee24fc13f319677f6f75fa3c9fb3c5f82a2
Hawk Ticehurst <39639992+hawkticehurst@users.noreply.github.com>
2021-03-24 16:23:57 -0700

    Update VSCode Breadcrumb Item Documentation & Stories (#47)
    Conduct the first polishing pass of the VSCode Breadcrumb Item component. Changes include:
    
    - Updating and adding to the VSCode Breadcrumb Item README documentation
    - Adding more Storybook controls and stories

--- src/breadcrumb-item/fixtures/createBreadcrumbItem.ts
+++ src/breadcrumb-item/fixtures/createBreadcrumbItem.ts
@@ -1,26 +1,67 @@
 import {VSCodeBreadcrumbItem} from '../index';
 import {focusObserver} from '../../utilities/storybook/index';
+import '../../../node_modules/vscode-codicons/dist/codicon.css';
 
 export type BreadcrumbItemArgs = {
-	label?: string;
-	href?: string;
+	label: string;
+	href: string;
+	customElement: boolean;
+	startIcon: boolean;
+	separatorIcon: boolean;
+	endIcon: boolean;
 	isFocused: boolean;
 };
 
 export function createBreadcrumbItem({
 	label,
 	href,
+	customElement,
+	startIcon,
+	separatorIcon,
+	endIcon,
 	isFocused,
 }: BreadcrumbItemArgs) {
 	const breadcrumbItem = new VSCodeBreadcrumbItem();
 
-	if (label) {
+	if (label && !customElement) {
 		breadcrumbItem.textContent = label;
+	}
+	breadcrumbItem.setAttribute('href', href);
+	if (customElement) {
+		const button = document.createElement('vscode-button');
+		button.textContent =
+			'Button Element (But This Could Be Any Valid HTML)';
+		breadcrumbItem.appendChild(button);
+	}
+	if (startIcon) {
+		const start = createCustomIcon('start', 'codicon-symbol-method');
+		breadcrumbItem.appendChild(start);
+	}
+	if (separatorIcon) {
+		const separator = createCustomIcon(
+			'separator',
+			'codicon-chevron-right'
+		);
+		breadcrumbItem.appendChild(separator);
+	}
+	if (endIcon) {
+		const end = createCustomIcon('end', 'codicon-symbol-method');
+		breadcrumbItem.appendChild(end);
 	}
 	if (isFocused) {
 		focusObserver(breadcrumbItem);
 	}
-	breadcrumbItem.setAttribute('href', href);
 
 	return breadcrumbItem;
+}
+
+export function createCustomIcon(
+	iconType: string,
+	iconName: string
+): HTMLElement {
+	const icon = document.createElement('svg');
+	icon.setAttribute('slot', iconType);
+	icon.classList.add('codicon');
+	icon.classList.add(iconName);
+	return icon;
 }
