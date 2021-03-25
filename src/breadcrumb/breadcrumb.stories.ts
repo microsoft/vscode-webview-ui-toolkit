1875e3afb3b536d0bf4104dd850205876f4cab9d
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-25 10:45:19 -0700

    Add brand new Storybook controls and stories to VSCode Breadcrumb and update createBreadcrumb fixtures function to reflect this change

--- src/breadcrumb/breadcrumb.stories.ts
+++ src/breadcrumb/breadcrumb.stories.ts
@@ -9,7 +9,11 @@ VSCodeDesignSystemProvider;
 export default {
 	title: 'Library/Breadcrumb',
 	argTypes: {
-		numberOfChildren: {control: 'number'},
+		breadcrumbItemList: {control: 'array'},
+		customElement: {control: 'boolean'},
+		startIcon: {control: 'boolean'},
+		separatorIcon: {control: 'boolean'},
+		endIcon: {control: 'boolean'},
 	},
 	parameters: {
 		actions: {
@@ -24,12 +28,70 @@ const Template = ({...args}: BreadcrumbArgs) => {
 
 export const Default: any = Template.bind({});
 Default.args = {
-	numberOfChildren: 3,
+	breadcrumbItemList: ['src', 'components', 'index.ts'],
+	customElement: false,
+	startIcon: false,
+	separatorIcon: false,
+	endIcon: false,
 };
 Default.parameters = {
 	docs: {
 		source: {
-			code: `<vscode-breadcrumb>\n\t<vscode-breadcrumb-item href="#">directory</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">directory</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">file</vscode-breadcrumb-item>\n</vscode-breadcrumb>`,
+			code: `<vscode-breadcrumb>\n\t<vscode-breadcrumb-item href="#">src</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">components</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">index.ts</vscode-breadcrumb-item>\n</vscode-breadcrumb>`,
+		},
+	},
+};
+
+export const WithCustomElements: any = Template.bind({});
+WithCustomElements.args = {
+	...Default.args,
+	customElement: true,
+};
+WithCustomElements.parameters = {
+	docs: {
+		source: {
+			code: `<vscode-breadcrumb>\n\t<vscode-breadcrumb-item href="#">\n\t\t<vscode-button>Button Element</vscode-button>\n\t</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">\n\t\t<vscode-button>Button Element</vscode-button>\n\t</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">\n\t\t<vscode-button>Button Element</vscode-button>\n\t</vscode-breadcrumb-item>\n</vscode-breadcrumb>`,
+		},
+	},
+};
+
+export const WithCustomSeparators: any = Template.bind({});
+WithCustomSeparators.args = {
+	...Default.args,
+	separatorIcon: true,
+};
+WithCustomSeparators.parameters = {
+	docs: {
+		source: {
+			code: `<!-- Note: Using VSCode Codicon Library -->\n\n<vscode-breadcrumb>\n\t<vscode-breadcrumb-item href="#">\n\t\tsrc\n\t\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n\t</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">\n\t\tcomponents\n\t\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n\t</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">\n\t\tindex.ts\n\t\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n\t</vscode-breadcrumb-item>\n</vscode-breadcrumb>`,
+		},
+	},
+};
+
+export const WithStartIcons: any = Template.bind({});
+WithStartIcons.args = {
+	...Default.args,
+	startIcon: true,
+	separatorIcon: true,
+};
+WithStartIcons.parameters = {
+	docs: {
+		source: {
+			code: `<!-- Note: Using VSCode Codicon Library -->\n\n<vscode-breadcrumb>\n\t<vscode-breadcrumb-item href="#">\n\t\tsrc\n\t\t<svg slot="start" class="codicon codicon-symbol-method"></svg>\n\t\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n\t</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">\n\t\tcomponents\n\t\t<svg slot="start" class="codicon codicon-symbol-method"></svg>\n\t\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n\t</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">\n\t\tindex.ts\n\t\t<svg slot="start" class="codicon codicon-symbol-method"></svg>\n\t\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n\t</vscode-breadcrumb-item>\n</vscode-breadcrumb>`,
+		},
+	},
+};
+
+export const WithEndIcons: any = Template.bind({});
+WithEndIcons.args = {
+	...Default.args,
+	separatorIcon: true,
+	endIcon: true,
+};
+WithEndIcons.parameters = {
+	docs: {
+		source: {
+			code: `<!-- Note: Using VSCode Codicon Library -->\n\n<vscode-breadcrumb>\n\t<vscode-breadcrumb-item href="#">\n\t\tsrc\n\t\t<svg slot="end" class="codicon codicon-symbol-method"></svg>\n\t\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n\t</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">\n\t\tcomponents\n\t\t<svg slot="end" class="codicon codicon-symbol-method"></svg>\n\t\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n\t</vscode-breadcrumb-item>\n\t<vscode-breadcrumb-item href="#">\n\t\tindex.ts\n\t\t<svg slot="end" class="codicon codicon-symbol-method"></svg>\n\t\t<svg slot="separator" class="codicon codicon-chevron-right"></svg>\n\t</vscode-breadcrumb-item>\n</vscode-breadcrumb>`,
 		},
 	},
 };
