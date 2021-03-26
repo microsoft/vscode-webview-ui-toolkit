04e5dd86b6aa6cdc4320f4a355e73777f7916ab7
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-26 13:19:16 -0700

    Add new Storybook controls and update the createCheckbox fixtures function to reflect those changes

--- src/checkbox/fixtures/createCheckbox.ts
+++ src/checkbox/fixtures/createCheckbox.ts
@@ -5,7 +5,10 @@ export type CheckboxArgs = {
 	label?: string;
 	isChecked: boolean;
 	isDisabled: boolean;
-	isFocused: boolean;
+	isAutoFocused: boolean;
+	isReadOnly: boolean;
+	isRequired: boolean;
+	hasValue: boolean;
 	onChange: any;
 };
 
@@ -13,7 +16,10 @@ export function createCheckbox({
 	label,
 	isChecked,
 	isDisabled,
-	isFocused,
+	isAutoFocused,
+	isReadOnly,
+	isRequired,
+	hasValue,
 	onChange,
 }: CheckboxArgs) {
 	const checkbox = new VSCodeCheckbox();
@@ -25,8 +31,19 @@ export function createCheckbox({
 	if (isDisabled) {
 		checkbox.setAttribute('disabled', '');
 	}
-	if (isFocused) {
+	if (isAutoFocused) {
+		checkbox.setAttribute('autofocus', '');
+		// Focus observer will force focus if checkbox focus is lost after page load
 		focusObserver(checkbox);
+	}
+	if (isReadOnly) {
+		checkbox.setAttribute('readonly', '');
+	}
+	if (isRequired) {
+		checkbox.setAttribute('required', '');
+	}
+	if (hasValue) {
+		checkbox.setAttribute('value', 'foo');
 	}
 	checkbox.addEventListener('change', onChange);
 
