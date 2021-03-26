07df07a3d8379c42f6ee0e1ac3a4647305256cd8
Hawk Ticehurst <39639992+hawkticehurst@users.noreply.github.com>
2021-03-26 14:30:52 -0700

    Add New Storybook Utility Function (#52)
    Implement `createCodiconIcon` Storybook utility function for creating and returning SVG elements that render VSCode Codicon icons.

--- src/button/fixtures/createButton.ts
+++ src/button/fixtures/createButton.ts
@@ -1,5 +1,8 @@
 import {ButtonAppearance, VSCodeButton} from '../index';
-import {focusObserver} from '../../utilities/storybook/index';
+import {
+	createCodiconIcon,
+	focusObserver,
+} from '../../utilities/storybook/index';
 import '../../../node_modules/vscode-codicons/dist/codicon.css';
 
 export type ButtonArgs = {
@@ -40,28 +43,18 @@ export function createButton({
 		focusObserver(button);
 	}
 	if (startIcon) {
-		const start = createCustomIcon('codicon-git-merge', 'start');
+		const start = createCodiconIcon('git-merge', 'start');
 		button.appendChild(start);
 	}
 	if (endIcon) {
-		const end = createCustomIcon('codicon-chevron-right', 'end');
+		const end = createCodiconIcon('chevron-right', 'end');
 		button.appendChild(end);
 	}
 	if (iconOnly) {
-		const icon = createCustomIcon('codicon-check');
+		const icon = createCodiconIcon('check');
 		button.appendChild(icon);
 	}
 	button.addEventListener('click', onClick);
 
 	return button;
-}
-
-function createCustomIcon(iconName: string, iconType?: string): HTMLElement {
-	const icon = document.createElement('svg');
-	if (iconType) {
-		icon.setAttribute('slot', iconType);
-	}
-	icon.classList.add('codicon');
-	icon.classList.add(iconName);
-	return icon;
 }
