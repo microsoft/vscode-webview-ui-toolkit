bcf9a14243c0169f180d8d3ef37264d6ff5bec9e
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-29 11:57:53 -0700

    Add new Storybook controls and update the createLink fixtures function to reflect those changes

--- src/link/fixtures/createLink.ts
+++ src/link/fixtures/createLink.ts
@@ -1,12 +1,19 @@
 import {LinkAppearance, VSCodeLink} from '../index';
-import {focusObserver} from '../../utilities/storybook/index';
+import {
+	createCodiconIcon,
+	focusObserver,
+} from '../../utilities/storybook/index';
+import '../../../node_modules/vscode-codicons/dist/codicon.css';
 
 export type LinkArgs = {
 	label: string;
 	href: string;
 	appearance?: LinkAppearance;
 	isDisabled: boolean;
-	isFocused: boolean;
+	isAutoFocused: boolean;
+	startIcon: boolean;
+	endIcon: boolean;
+	iconOnly: boolean;
 };
 
 export function createLink({
@@ -14,11 +21,16 @@ export function createLink({
 	href,
 	appearance,
 	isDisabled,
-	isFocused,
+	isAutoFocused,
+	startIcon,
+	endIcon,
+	iconOnly,
 }: LinkArgs) {
 	const link = new VSCodeLink();
 
-	link.textContent = label;
+	if (label && !iconOnly) {
+		link.textContent = label;
+	}
 	if (href) {
 		link.setAttribute('href', href);
 	}
@@ -28,8 +40,22 @@ export function createLink({
 	if (isDisabled) {
 		link.setAttribute('disabled', '');
 	}
-	if (isFocused) {
+	if (isAutoFocused) {
+		link.setAttribute('autofocus', '');
+		// Focus observer will force focus if button focus is lost after page load
 		focusObserver(link);
+	}
+	if (startIcon) {
+		const start = createCodiconIcon('git-merge', 'start');
+		link.appendChild(start);
+	}
+	if (endIcon) {
+		const end = createCodiconIcon('chevron-right', 'end');
+		link.appendChild(end);
+	}
+	if (iconOnly) {
+		const icon = createCodiconIcon('check');
+		link.appendChild(icon);
 	}
 
 	return link;
--- src/link/link.stories.ts
+++ src/link/link.stories.ts
@@ -18,7 +18,10 @@ export default {
 			},
 		},
 		isDisabled: {control: 'boolean'},
-		isFocused: {control: 'boolean'},
+		isAutoFocused: {control: 'boolean'},
+		startIcon: {control: 'boolean'},
+		endIcon: {control: 'boolean'},
+		iconOnly: {control: 'boolean'},
 	},
 	parameters: {
 		actions: {
@@ -33,16 +36,19 @@ const Template = ({...args}: LinkArgs) => {
 
 export const Default: any = Template.bind({});
 Default.args = {
-	label: 'Link',
+	label: 'Link Text',
 	href: '#',
 	appearance: 'Link',
 	isDisabled: false,
-	isFocused: false,
+	isAutoFocused: false,
+	startIcon: false,
+	endIcon: false,
+	iconOnly: false,
 };
 Default.parameters = {
 	docs: {
 		source: {
-			code: `<vscode-link href="#">Link</vscode-link>`,
+			code: `<vscode-link href="#">Link Text</vscode-link>`,
 		},
 	},
 };
@@ -55,7 +61,7 @@ Primary.args = {
 Primary.parameters = {
 	docs: {
 		source: {
-			code: `<vscode-link appearance="primary" href="#">Link</vscode-link>`,
+			code: `<vscode-link href="#" appearance="primary">Link Text</vscode-link>`,
 		},
 	},
 };
@@ -68,20 +74,7 @@ Secondary.args = {
 Secondary.parameters = {
 	docs: {
 		source: {
-			code: `<vscode-link appearance="secondary" href="#">Link</vscode-link>`,
-		},
-	},
-};
-
-export const Install: any = Template.bind({});
-Install.args = {
-	...Default.args,
-	appearance: 'Install',
-};
-Install.parameters = {
-	docs: {
-		source: {
-			code: `<vscode-link appearance="install" href="#">Link</vscode-link>`,
+			code: `<vscode-link href="#" appearance="secondary">Link Text</vscode-link>`,
 		},
 	},
 };
@@ -94,7 +87,85 @@ Text.args = {
 Text.parameters = {
 	docs: {
 		source: {
-			code: `<vscode-link appearance="text" href="#">Link</vscode-link>`,
+			code: `<vscode-link href="#" appearance="text">Link Text</vscode-link>`,
+		},
+	},
+};
+
+export const Install: any = Template.bind({});
+Install.args = {
+	...Default.args,
+	appearance: 'Install',
+};
+Install.parameters = {
+	docs: {
+		source: {
+			code: `<vscode-link href="#" appearance="install">Link Text</vscode-link>`,
+		},
+	},
+};
+
+export const WithAutofocus: any = Template.bind({});
+WithAutofocus.args = {
+	...Default.args,
+	isAutoFocused: true,
+};
+WithAutofocus.parameters = {
+	docs: {
+		source: {
+			code: `<vscode-link href="#" autofocus>Link Text</vscode-link>`,
+		},
+	},
+};
+
+export const WithDisabled: any = Template.bind({});
+WithDisabled.args = {
+	...Default.args,
+	isDisabled: true,
+};
+WithDisabled.parameters = {
+	docs: {
+		source: {
+			code: `<vscode-link href="#" disabled>Link Text</vscode-link>`,
+		},
+	},
+};
+
+export const WithStartIcon: any = Template.bind({});
+WithStartIcon.args = {
+	...Default.args,
+	startIcon: true,
+};
+WithStartIcon.parameters = {
+	docs: {
+		source: {
+			code: `<!-- Note: Using VSCode Codicon Library -->\n\n<vscode-link href="#">\n\tLink Text\n\t<svg slot="start" class="codicon codicon-git-merge"></svg>\n</vscode-link>`,
+		},
+	},
+};
+
+export const WithEndIcon: any = Template.bind({});
+WithEndIcon.args = {
+	...Default.args,
+	endIcon: true,
+};
+WithEndIcon.parameters = {
+	docs: {
+		source: {
+			code: `<!-- Note: Using VSCode Codicon Library -->\n\n<vscode-link href="#">\n\tLink Text\n\t<svg slot="end" class="codicon codicon-chevron-right"></svg>\n</vscode-link>`,
+		},
+	},
+};
+
+export const WithIconOnly: any = Template.bind({});
+WithIconOnly.args = {
+	...Default.args,
+	iconOnly: true,
+};
+WithIconOnly.parameters = {
+	docs: {
+		source: {
+			code: `<!-- Note: Using VSCode Codicon Library -->\n\n<vscode-link href="#">\n\t<svg class="codicon codicon-check"></svg>\n</vscode-link>`,
 		},
 	},
 };
