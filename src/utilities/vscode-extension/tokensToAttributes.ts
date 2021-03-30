61dff6eabeb1d6ae7c8975a2b535adbc67c2e74d
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-30 15:22:37 -0700

    Update the colorTokensToAttributeNames object to reflect the current VSCode Design System

--- src/utilities/vscode-extension/tokensToAttributes.ts
+++ src/utilities/vscode-extension/tokensToAttributes.ts
@@ -3,7 +3,21 @@
  * associated attributes that the `vscode-design-system-provider` defines
  */
 export const colorTokensToAttributeNames: any = {
-	'--vscode-button-background': 'accent-base-color',
-	'--vscode-button-hoverBackground': 'accent-hover-color',
-	/* Insert more color token attribute name mappings */
+	'--vscode-focusBorder': 'focus-border-color',
+	'--vscode-button-background': 'button-primary-background-color',
+	'--vscode-button-foreground': 'button-primary-foreground-color',
+	'--vscode-button-hoverBackground': 'button-primary-hover-background-color',
+	'--vscode-button-secondaryBackground': 'button-secondary-background-color',
+	'--vscode-button-secondaryForeground': 'button-secondary-foreground-color',
+	'--vscode-button-secondaryHoverBackground': 'button-secondary-hover-background-color',
+	'--vscode-textLink-foreground': 'text-link-foreground-color',
+	'--vscode-checkbox-background': 'checkbox-background-color',
+	'--vscode-checkbox-foreground': 'checkbox-foreground-color',
+	'--vscode-checkbox-border': 'checkbox-border-color',
+	'--vscode-breadcrumb-background': 'breadcrumb-item-background-color',
+	'--vscode-breadcrumb-foreground': 'breadcrumb-item-foreground-color',
+	'--vscode-breadcrumb-focusForeground': 'breadcrumb-item-foreground-hover-color',
+	// '--vscode-?': 'select-background-color',
+	// '--vscode-?': 'select-foreground-color',
+	// '--vscode-?': 'select-border-color',
 };
