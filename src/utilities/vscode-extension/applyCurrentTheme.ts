7aa2b8f26c56e7204ac506efac0aef94eec2ced0
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-30 14:53:46 -0700

    Rename setVSCodeThemeOnDesignProvider.ts to applyCurrentTheme.ts

---
+++ src/utilities/vscode-extension/applyCurrentTheme.ts
@@ -0,0 +1,41 @@
+import {colorTokensToAttributeNames} from './tokensToAttributes';
+
+/**
+ * Applies the current VSCode theme to the VSCode Webview Toolkit
+ * components.
+ *
+ * Note: This utility function should be used in tandem with the
+ * `setThemeEventListener` utility function to correctly set up
+ * VSCode theme change handling in the VSCode Webview Toolkit.
+ *
+ * @param designProvider A reference to the `<vscode-design-system-provider>` element
+ */
+export function applyCurrentTheme(designProvider: HTMLElement) {
+	// Get all the styles applied to the <body> tag in the webview HTML
+	// Importantly this includes all the CSS variables associated with the
+	// current VSCode theme
+	const styles = getComputedStyle(document.body);
+
+	if (designProvider) {
+		setVSCodeThemeOnDesignProvider(styles, designProvider);
+
+		// Listen for messages from the setThemeEventListener utility function
+		window.addEventListener('message', event => {
+			if (event.data.didThemeChange) {
+				setVSCodeThemeOnDesignProvider(styles, designProvider);
+			}
+		});
+	}
+}
+
+function setVSCodeThemeOnDesignProvider(
+	styles: CSSStyleDeclaration,
+	designProvider: HTMLElement
+) {
+	for (const colorToken in colorTokensToAttributeNames) {
+		const attributeName = colorTokensToAttributeNames[colorToken];
+		const tokenValue = styles.getPropertyValue(colorToken).toString();
+		// Set a given VSCode theme color to its respective <vscode-design-system-provider> attribute
+		designProvider.setAttribute(attributeName, tokenValue);
+	}
+}
