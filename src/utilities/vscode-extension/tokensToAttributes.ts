7618b427ea304b98202ccd68ce0fe7a3f5bf0d9d
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-03-30 14:55:46 -0700

    Pull the colorTokensToAttributeNames object into a separate file

---
+++ src/utilities/vscode-extension/tokensToAttributes.ts
@@ -0,0 +1,9 @@
+/**
+ * A mapping of all the VSCode theme CSS variables mapped to the
+ * associated attributes that the `vscode-design-system-provider` defines
+ */
+export const colorTokensToAttributeNames: any = {
+	'--vscode-button-background': 'accent-base-color',
+	'--vscode-button-hoverBackground': 'accent-hover-color',
+	/* Insert more color token attribute name mappings */
+};
