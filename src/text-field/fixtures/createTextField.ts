6cd912398d7857ad03835cbfa001ec5a7e1cd312
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-04-07 15:07:28 -0700

    Add new Storybook controls and update the createTextField fixtures function to reflect those changes

--- src/text-field/fixtures/createTextField.ts
+++ src/text-field/fixtures/createTextField.ts
@@ -1,27 +1,37 @@
 import {VSCodeTextField} from '../index';
+import {createCodiconIcon} from '../../utilities/storybook/index';
+import '../../../node_modules/vscode-codicons/dist/codicon.css';
 
 export type TextFieldArgs = {
 	label?: string;
 	placeholder?: string;
 	value?: string;
+	pattern?: string;
 	minLength?: number;
 	maxLength?: number;
+	size?: number;
 	isRequired?: boolean;
 	isReadOnly?: boolean;
 	isDisabled?: boolean;
 	isAutoFocused?: boolean;
+	startIcon: boolean;
+	endIcon: boolean;
 };
 
 export function createTextField({
 	label,
 	placeholder,
 	value,
+	pattern,
 	minLength,
 	maxLength,
+	size,
 	isRequired,
 	isReadOnly,
 	isDisabled,
 	isAutoFocused,
+	startIcon,
+	endIcon,
 }: TextFieldArgs) {
 	const textField = new VSCodeTextField();
 
@@ -34,24 +44,37 @@ export function createTextField({
 	if (value) {
 		textField.value = value;
 	}
+	if (pattern) {
+		textField.setAttribute('pattern', pattern);
+	}
 	if (minLength) {
 		textField.setAttribute('minlength', minLength.toString());
 	}
 	if (maxLength) {
 		textField.setAttribute('maxlength', maxLength.toString());
 	}
+	if (size) {
+		textField.setAttribute('size', size.toString());
+	}
 	if (isRequired) {
 		textField.setAttribute('required', '');
 	}
 	if (isReadOnly) {
 		textField.setAttribute('readonly', '');
-		textField.setAttribute('value', 'Read Only Text');
 	}
 	if (isDisabled) {
 		textField.setAttribute('disabled', '');
 	}
 	if (isAutoFocused) {
 		textField.setAttribute('autofocus', '');
+	}
+	if (startIcon) {
+		const start = createCodiconIcon('search', 'start');
+		textField.appendChild(start);
+	}
+	if (endIcon) {
+		const end = createCodiconIcon('text-size', 'end');
+		textField.appendChild(end);
 	}
 
 	return textField;
