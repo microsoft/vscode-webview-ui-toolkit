ea651e5a15754c0e9088e468c631b93f4433a19b
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-04-12 15:12:58 -0700

    ImplemenPanel fixtures function

---
+++ src/tab-panel/fixtures/createTabPanel.ts
@@ -0,0 +1,33 @@
+import {VSCodeTabPanel} from '../index';
+
+export type TabPanelArgs = {
+	label?: string;
+	hasComplexContent?: boolean;
+};
+
+export function createTabPanel({label, hasComplexContent}: TabPanelArgs) {
+	const tabPanel = new VSCodeTabPanel();
+
+	if (label && !hasComplexContent) {
+		tabPanel.textContent = label;
+	}
+	if (hasComplexContent) {
+		const emojis = ['🍓', '🍎', '🍊', '🍍', '🥭', '🍋', '🍇'];
+		const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+		tabPanel.innerHTML = `
+        <section style="display: flex; flex-direction: column; width: 100%;">
+            <h1>Smoothie Maker ${randomEmoji}</h1>
+            <vscode-checkbox>Apples</vscode-checkbox>
+            <vscode-checkbox>Oranges</vscode-checkbox>
+            <vscode-checkbox>Grapes</vscode-checkbox>
+            <vscode-checkbox disabled>Blueberries</vscode-checkbox>
+            <vscode-checkbox>Pineapple</vscode-checkbox>
+            <vscode-checkbox>Mango</vscode-checkbox>
+            <vscode-checkbox>Lemon</vscode-checkbox>
+            <vscode-button style="margin-top: 1rem;">Make Smoothie!</vscode-button>
+        </section>
+        `;
+	}
+
+	return tabPanel;
+}
