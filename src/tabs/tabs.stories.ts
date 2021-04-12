7132adaed0cc68933818108293a995d2c94f4e32
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-04-12 14:10:53 -0700

    Add new VSCode Tabs stories

--- src/tabs/tabs.stories.ts
+++ src/tabs/tabs.stories.ts
@@ -23,6 +23,7 @@ export default {
 				options: ['Horizontal', 'Vertical'],
 			},
 		},
+		tabMetaData: {control: 'array'},
 	},
 	parameters: {
 		actions: {
@@ -39,6 +40,12 @@ export const Default: any = Template.bind({});
 Default.args = {
 	activeTab: 'Terminal',
 	orientation: 'Horizontal',
+	tabMetaData: [
+		{title: 'PROBLEMS', isDisabled: false, customIcon: false},
+		{title: 'OUTPUT', isDisabled: false, customIcon: false},
+		{title: 'DEBUG CONSOLE', isDisabled: false, customIcon: false},
+		{title: 'TERMINAL', isDisabled: false, customIcon: false},
+	],
 };
 Default.parameters = {
 	docs: {
@@ -48,12 +55,35 @@ Default.parameters = {
 	},
 };
 
-export const Vertical: any = Template.bind({});
-Vertical.args = {
-	activeTab: 'Terminal',
-	orientation: 'Vertical',
+export const WithDisabled: any = Template.bind({});
+WithDisabled.args = {
+	...Default.args,
+	tabMetaData: [
+		{title: 'PROBLEMS', isDisabled: true, customIcon: false},
+		{title: 'OUTPUT', isDisabled: false, customIcon: false},
+		{title: 'DEBUG CONSOLE', isDisabled: true, customIcon: false},
+		{title: 'TERMINAL', isDisabled: false, customIcon: false},
+	],
 };
-Vertical.parameters = {
+WithDisabled.parameters = {
+	docs: {
+		source: {
+			code: `<vscode-tabs activeid="tab-4" orientation="vertical">\n\t<vscode-tab id="tab-1" disabled>PROBLEMS</vscode-tab>\n\t<vscode-tab id="tab-2">OUTPUT</vscode-tab>\n\t<vscode-tab id="tab-3" disabled>DEBUG CONSOLE</vscode-tab>\n\t<vscode-tab id="tab-4">TERMINAL</vscode-tab>\n\t<vscode-tab-panel id="tab-panel-1">\n\t\tProblems content.\n\t</vscode-tab-panel>\n\t<vscode-tab-panel id="tab-panel-2">\n\t\tOutput content.\n\t</vscode-tab-panel>\n\t<vscode-tab-panel id="tab-panel-3">\n\t\tDebug content.\n\t</vscode-tab-panel>\n\t<vscode-tab-panel id="tab-panel-4">\n\t\tTerminal content.\n\t</vscode-tab-panel>\n</vscode-tabs>`,
+		},
+	},
+};
+
+export const WithCustomTabIcons: any = Template.bind({});
+WithCustomTabIcons.args = {
+	...Default.args,
+	tabMetaData: [
+		{title: 'PROBLEMS', isDisabled: false, customIcon: true},
+		{title: 'OUTPUT', isDisabled: false, customIcon: true},
+		{title: 'DEBUG CONSOLE', isDisabled: false, customIcon: true},
+		{title: 'TERMINAL', isDisabled: false, customIcon: true},
+	],
+};
+WithCustomTabIcons.parameters = {
 	docs: {
 		source: {
 			code: `<vscode-tabs activeid="tab-4" orientation="vertical">\n\t<vscode-tab id="tab-1">PROBLEMS</vscode-tab>\n\t<vscode-tab id="tab-2">OUTPUT</vscode-tab>\n\t<vscode-tab id="tab-3">DEBUG CONSOLE</vscode-tab>\n\t<vscode-tab id="tab-4">TERMINAL</vscode-tab>\n\t<vscode-tab-panel id="tab-panel-1">\n\t\tProblems content.\n\t</vscode-tab-panel>\n\t<vscode-tab-panel id="tab-panel-2">\n\t\tOutput content.\n\t</vscode-tab-panel>\n\t<vscode-tab-panel id="tab-panel-3">\n\t\tDebug content.\n\t</vscode-tab-panel>\n\t<vscode-tab-panel id="tab-panel-4">\n\t\tTerminal content.\n\t</vscode-tab-panel>\n</vscode-tabs>`,
