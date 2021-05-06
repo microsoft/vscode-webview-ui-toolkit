4266c2e1b9556e8374220e89406fcf86abe2e5fa
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-05-06 12:45:02 -0700

    Combine panels, panel-tab, and panel-view component implmentation into one index.ts file

--- src/panels/index.ts
+++ src/panels/index.ts
@@ -1,23 +1,75 @@
 import {customElement} from '@microsoft/fast-element';
-import {Tabs, TabsTemplate as template} from '@microsoft/fast-foundation';
-import {TabsStyles as styles} from './panel.styles';
+import {
+	Tab,
+	TabPanel,
+	TabPanelTemplate,
+	Tabs,
+	TabsOrientation,
+	TabsTemplate,
+	TabTemplate,
+} from '@microsoft/fast-foundation';
+import {PanelsStyles} from './panels.styles';
+import {PanelTabStyles} from './panel-tab.styles';
+import {PanelViewStyles} from './panel-view.styles';
 
 /**
- * The VS Code Tabs element. Extends
+ * The VS Code Panels element. Extends
  * {@link https://www.fast.design/docs/api/fast-foundation.tabs/ | Tabs} and
  * {@link https://www.fast.design/docs/api/fast-foundation.tabstemplate/ | TabsTemplate}.
  *
  * @remarks
- * HTML Element: `<vscode-tabs>`
+ * HTML Element: `<vscode-panels>`
  *
  * @public
  */
 @customElement({
-	name: 'vscode-tabs',
-	template,
-	styles,
+	name: 'vscode-panels',
+	template: TabsTemplate,
+	styles: PanelsStyles,
 })
-export class VSCodeTabs extends Tabs {}
+export class VSCodePanels extends Tabs {
+	public connectedCallback() {
+		super.connectedCallback();
 
-export * from '../panel-tab/index';
-export * from '../panel-view/index';
+		// This will override any usage of the orientation attribute
+		// provided by the FAST Foundation Tabs component so that
+		// VSCodePanels are always oriented horizontally
+		if (this.orientation) {
+			this.orientation = TabsOrientation.horizontal;
+		}
+	}
+}
+
+/**
+ * The VS Code Panel Tab element. Extends
+ * {@link https://www.fast.design/docs/api/fast-foundation.tab/ | Tab} and
+ * {@link https://www.fast.design/docs/api/fast-foundation.tabtemplate/ | TabTemplate}.
+ *
+ * @remarks
+ * HTML Element: `<vscode-panel-tab>`
+ *
+ * @public
+ */
+@customElement({
+	name: 'vscode-panel-tab',
+	template: TabTemplate,
+	styles: PanelTabStyles,
+})
+export class VSCodePanelTab extends Tab {}
+
+/**
+ * The VS Code Panel View element. Extends
+ * {@link https://www.fast.design/docs/api/fast-foundation.tabpanel/ | TabPanel} and
+ * {@link https://www.fast.design/docs/api/fast-foundation.tabpaneltemplate/ | TabPanelTemplate}.
+ *
+ * @remarks
+ * HTML Element: `<vscode-panel-view>`
+ *
+ * @public
+ */
+@customElement({
+	name: 'vscode-panel-view',
+	template: TabPanelTemplate,
+	styles: PanelViewStyles,
+})
+export class VSCodePanelView extends TabPanel {}
