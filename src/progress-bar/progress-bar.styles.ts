d18bc44013c69484067e0347e313eac4c3c602e4
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-05-19 11:45:23 -0700

    Run prettier on all files

--- src/progress-bar/progress-bar.styles.ts
+++ src/progress-bar/progress-bar.styles.ts
@@ -1,6 +1,10 @@
 import {css} from '@microsoft/fast-element';
 import {display} from '@microsoft/fast-foundation';
-import {cornerRadius, designUnit, progressBackgroundColor} from '../design-tokens';
+import {
+	cornerRadius,
+	designUnit,
+	progressBackgroundColor,
+} from '../design-tokens';
 
 export const ProgressBarStyles = css`
 	${display('flex')} :host {
