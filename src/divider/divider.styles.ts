edaafbc54457a89574c2723ef2215690a55456c1
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-06-18 09:25:21 -0700

    Update divider styles

--- src/divider/divider.styles.ts
+++ src/divider/divider.styles.ts
@@ -7,10 +7,11 @@ import {borderWidth, designUnit} from '../design-tokens';
 
 export const DividerStyles = css`
 	${display('block')} :host {
+		border: none;
+		border-top: calc(${borderWidth} * 1px) solid #454545;
 		box-sizing: content-box;
 		height: 0;
 		margin: calc(${designUnit} * 1px) 0;
-		border: none;
-		border-top: solid calc(${borderWidth} * 1px) #454545;
+		width: 100%;
 	}
 `;
