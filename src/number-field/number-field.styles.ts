c2b863ec45c7267e6c5cb7c8c52174bd3200b1b6
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-06-29 15:09:54 -0700

    Update number field so that it imports Text Field stylings as a dependency to its own styling

--- src/number-field/number-field.styles.ts
+++ src/number-field/number-field.styles.ts
@@ -2,96 +2,12 @@
 // Licensed under the MIT License.
 
 import {css} from '@microsoft/fast-element';
-import {
-	disabledCursor,
-	display,
-	focusVisible,
-} from '@microsoft/fast-foundation';
-import {
-	borderWidth,
-	cornerRadius,
-	designUnit,
-	disabledOpacity,
-	focusBorder,
-	fontFamily,
-	inputBackground,
-	inputBorder,
-	inputForeground,
-	inputHeight,
-	inputMinWidth,
-	inputPlaceholderForeground,
-	typeRampBaseFontSize,
-	typeRampBaseLineHeight,
-} from '../design-tokens';
+import {designUnit, inputForeground} from '../design-tokens';
+import {TextFieldStyles} from '../text-field/text-field.styles';
 
 export const NumberFieldStyles = css`
-	${display('inline-block')} :host {
-		font-family: ${fontFamily};
-		outline: none;
-		user-select: none;
-	}
-	.root {
-		box-sizing: border-box;
-		position: relative;
-		display: flex;
-		flex-direction: row;
-		color: ${inputForeground};
-		background: ${inputBackground};
-		border-radius: calc(${cornerRadius} * 1px);
-		border: calc(${borderWidth} * 1px) solid ${inputBorder};
-		height: calc(${inputHeight} * 1px);
-		min-width: ${inputMinWidth};
-	}
-	.control {
-		-webkit-appearance: none;
-		font: inherit;
-		background: transparent;
-		border: 0;
-		color: inherit;
-		height: calc(100% - (${designUnit} * 1px));
-		width: 100%;
-		margin-top: auto;
-		margin-bottom: auto;
-		border: none;
-		padding: 0 calc(${designUnit} * 2px + 1px);
-		font-size: ${typeRampBaseFontSize};
-		line-height: ${typeRampBaseLineHeight};
-	}
-	.control:hover,
-	.control:${focusVisible},
-	.control:disabled,
-	.control:active {
-		outline: none;
-	}
-	.label {
-		display: block;
-		color: ${inputPlaceholderForeground};
-		cursor: pointer;
-		font-size: ${typeRampBaseFontSize};
-		line-height: ${typeRampBaseLineHeight};
-		margin-bottom: calc(${designUnit} * 1px);
-	}
-	.label__hidden {
-		display: none;
-		visibility: hidden;
-	}
-	.start,
-	.end {
-		display: flex;
-		margin: auto;
-		fill: currentcolor;
-	}
-	::slotted(svg),
-	::slotted(span) {
-		width: calc(${designUnit} * 4px);
-		height: calc(${designUnit} * 4px);
-	}
-	.start {
-		margin-inline-start: 11px;
-	}
-	.end {
-		margin-inline-end: 11px;
-	}
+	${TextFieldStyles}
+
 	.step-up,
 	.step-down {
 		padding: 0 calc(${designUnit} * 2px);
@@ -113,28 +29,5 @@ export const NumberFieldStyles = css`
 	.step-down:before {
 		border-top-color: ${inputForeground};
 		margin-top: 2px;
-	}
-	:host(:hover:not([disabled])) .root {
-		background: ${inputBackground};
-		border-color: ${inputBorder};
-	}
-	:host(:active:not([disabled])) .root {
-		background: ${inputBackground};
-		border-color: ${focusBorder};
-	}
-	:host(:focus-within:not([disabled])) .root {
-		border-color: ${focusBorder};
-	}
-	:host([disabled]) .label,
-	:host([readonly]) .label,
-	:host([readonly]) .control,
-	:host([disabled]) .control {
-		cursor: ${disabledCursor};
-	}
-	:host([disabled]) {
-		opacity: ${disabledOpacity};
-	}
-	:host([disabled]) .control {
-		border-color: ${inputBorder};
 	}
 `;
