40679f402836cd6b365fc096400dff28e6205fb7
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-06-30 16:19:45 -0700

    Remove random emoji implementation from createPanelView fixture function

--- src/panels/fixtures/createPanelView.ts
+++ src/panels/fixtures/createPanelView.ts
@@ -15,11 +15,9 @@ export function createPanelView({label, hasComplexContent}: PanelViewArgs) {
 		panelView.textContent = label;
 	}
 	if (hasComplexContent) {
-		const emojis = ['🍓', '🍎', '🍊', '🍍', '🥭', '🍋', '🍇'];
-		const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
 		panelView.innerHTML = /*html*/ `
             <section style="display: flex; flex-direction: column; width: 100%;">
-                <h1 style="margin-top: 0;">Smoothie Maker ${randomEmoji}</h1>
+                <h1 style="margin-top: 0;">Smoothie Maker 🍓</h1>
                 <vscode-checkbox>Apples</vscode-checkbox>
                 <vscode-checkbox>Oranges</vscode-checkbox>
                 <vscode-checkbox>Grapes</vscode-checkbox>
