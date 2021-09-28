abf0f72235db1ed23bcd71be3bc18ca7823e30e1
David Dossett <ddossett@microsoft.com>
2021-09-28 14:53:24 -0700

    Initial commit

--- src/checkbox/index.ts
+++ src/checkbox/index.ts
@@ -21,4 +21,20 @@ import {CheckboxStyles as styles} from './checkbox.styles';
 	template,
 	styles,
 })
-export class VSCodeCheckbox extends Checkbox {}
+export class VSCodeCheckbox extends Checkbox {
+	/**
+	 * Component lifecycle method that runs when the component is inserted
+	 * into the DOM.
+	 *
+	 * @internal
+	 */
+	public connectedCallback() {
+		super.connectedCallback();
+		if (this.textContent) {
+			this.setAttribute('aria-label', this.textContent);
+		} else {
+			// Fallback to the label if there is no text content
+			this.setAttribute('aria-label', 'Checkbox');
+		}
+	}
+}
