73228535e553bc72167991373dd70f213c0d0ac0
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-10-01 13:50:35 -0700

    Update data grid documentation to with aria label guidance

--- src/data-grid/data-grid.stories.ts
+++ src/data-grid/data-grid.stories.ts
@@ -15,6 +15,7 @@ export default {
 		gridTemplateColumns: {control: 'text'},
 		hasCustomTitles: {control: 'boolean'},
 		gridData: {control: 'object'},
+		ariaLabel: {control: 'text'},
 	},
 	parameters: {
 		actions: {
@@ -52,11 +53,12 @@ Default.args = {
 			Header4: 'Cell Data',
 		},
 	],
+	ariaLabel: 'Default',
 };
 Default.parameters = {
 	docs: {
 		source: {
-			code: `<!-- HTML -->\n\n<vscode-data-grid id="basic-grid"></vscode-data-grid>\n\n// TypeScript or JavaScript\n\ndocument.getElementById('basic-grid').rowsData = [\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n];`,
+			code: `<!-- HTML -->\n\n<vscode-data-grid id="basic-grid" aria-label="Default"></vscode-data-grid>\n\n// TypeScript or JavaScript\n\ndocument.getElementById('basic-grid').rowsData = [\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n];`,
 		},
 	},
 };
@@ -217,11 +219,12 @@ WithStickyHeader.args = {
 			Header4: 'Cell Data',
 		},
 	],
+	ariaLabel: 'Sticky Header',
 };
 WithStickyHeader.parameters = {
 	docs: {
 		source: {
-			code: `<!-- HTML -->\n\n<vscode-data-grid id="basic-grid" generate-header="sticky"></vscode-data-grid>\n\n// TypeScript or JavaScript\n\ndocument.getElementById('basic-grid').rowsData = [\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t// Insert more rows ...\n];`,
+			code: `<!-- HTML -->\n\n<vscode-data-grid id="basic-grid" generate-header="sticky" aria-label="Sticky Header"></vscode-data-grid>\n\n// TypeScript or JavaScript\n\ndocument.getElementById('basic-grid').rowsData = [\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t// Insert more rows ...\n];`,
 		},
 	},
 };
@@ -230,11 +233,12 @@ export const WithCustomColumnWidths: any = Template.bind({});
 WithCustomColumnWidths.args = {
 	...Default.args,
 	gridTemplateColumns: '100px 10vw 3fr 30%',
+	ariaLabel: 'Custom Column Widths',
 };
 WithCustomColumnWidths.parameters = {
 	docs: {
 		source: {
-			code: `<!-- HTML -->\n\n<vscode-data-grid id="basic-grid" grid-template-columns="100px 10vw 3fr 30%"></vscode-data-grid>\n\n// TypeScript or JavaScript\n\ndocument.getElementById('basic-grid').rowsData = [\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n];`,
+			code: `<!-- HTML -->\n\n<vscode-data-grid id="basic-grid" grid-template-columns="100px 10vw 3fr 30%" aria-label="Custom Column Widths"></vscode-data-grid>\n\n// TypeScript or JavaScript\n\ndocument.getElementById('basic-grid').rowsData = [\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n];`,
 		},
 	},
 };
@@ -243,11 +247,12 @@ export const WithCustomTitles: any = Template.bind({});
 WithCustomTitles.args = {
 	...Default.args,
 	hasCustomTitles: true,
+	ariaLabel: 'Custom Column Titles',
 };
 WithCustomTitles.parameters = {
 	docs: {
 		source: {
-			code: `<!-- HTML -->\n\n<vscode-data-grid id="basic-grid"></vscode-data-grid>\n\n// TypeScript or JavaScript\n\ndocument.getElementById('basic-grid').rowsData = [\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n];\ndocument.getElementById('basic-grid').columnDefinitions = [\n\t{title: 'A Custom Header Title', columnDataKey: 'Header1'},\n\t{title: 'Another Custom Title', columnDataKey: 'Header2'},\n\t{title: 'Title Is Custom', columnDataKey: 'Header3'},\n\t{title: 'Custom Title', columnDataKey: 'Header4'},\n];`,
+			code: `<!-- HTML -->\n\n<vscode-data-grid id="basic-grid" aria-label="Custom Column Titles"></vscode-data-grid>\n\n// TypeScript or JavaScript\n\ndocument.getElementById('basic-grid').rowsData = [\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n\t{Header1: 'Cell Data', Header2: 'Cell Data', Header3: 'Cell Data', Header4: 'Cell Data'},\n];\ndocument.getElementById('basic-grid').columnDefinitions = [\n\t{title: 'A Custom Header Title', columnDataKey: 'Header1'},\n\t{title: 'Another Custom Title', columnDataKey: 'Header2'},\n\t{title: 'Title Is Custom', columnDataKey: 'Header3'},\n\t{title: 'Custom Title', columnDataKey: 'Header4'},\n];`,
 		},
 	},
 };
