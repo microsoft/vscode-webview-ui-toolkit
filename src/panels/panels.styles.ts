fcc594b84e5987337bbe7bd679324e3a5d421b47
David Dossett <ddossett@microsoft.com>
2021-10-19 13:44:36 -0700

    Overflow panel content on x axis as needed

--- src/panels/panels.styles.ts
+++ src/panels/panels.styles.ts
@@ -22,6 +22,7 @@ export const PanelsStyles = css`
 		color: ${foreground};
 		grid-template-columns: auto 1fr auto;
 		grid-template-rows: auto 1fr;
+		overflow-x: auto;
 	}
 	.tablist {
 		display: grid;
