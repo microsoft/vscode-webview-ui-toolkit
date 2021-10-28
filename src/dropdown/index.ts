f69cde1d36a861a1faf6a4471affc6c62268f1e9
Hawk Ticehurst <39639992+hawkticehurst@users.noreply.github.com>
2021-10-28 15:52:18 -0700

    Update Default Component Icons To Use Codicon Icons (#280)
    Description of changes
    
    Update the default checkbox checkedIndicator and dropdown indicator to use Codicon icons.

--- src/dropdown/index.ts
+++ src/dropdown/index.ts
@@ -34,14 +34,19 @@ export const vsCodeDropdown = Dropdown.compose<DropdownOptions>({
 	template,
 	styles,
 	indicator: `
-		<svg
+		<svg 
 			class="select-indicator"
 			part="select-indicator"
-			viewBox="0 0 12 7"
-			xmlns="http://www.w3.org/2000/svg"
+			width="16" 
+			height="16" 
+			viewBox="0 0 16 16" 
+			xmlns="http://www.w3.org/2000/svg" 
+			fill="currentColor"
 		>
-			<path
-				d="M11.85.65c.2.2.2.5 0 .7L6.4 6.84a.55.55 0 01-.78 0L.14 1.35a.5.5 0 11.71-.7L6 5.8 11.15.65c.2-.2.5-.2.7 0z"
+			<path 
+				fill-rule="evenodd" 
+				clip-rule="evenodd" 
+				d="M7.976 10.072l4.357-4.357.62.618L8.284 11h-.618L3 6.333l.619-.618 4.357 4.357z"
 			/>
 		</svg>
 	`,
