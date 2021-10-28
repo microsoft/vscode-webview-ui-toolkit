e092f220db88dbf54345519e700979ffa981de19
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-10-28 15:20:35 -0700

    Update checkbox checkedIndicator to use the Codicon check icon

--- src/checkbox/index.ts
+++ src/checkbox/index.ts
@@ -44,16 +44,19 @@ export const vsCodeCheckbox = Checkbox.compose<CheckboxOptions>({
 	template,
 	styles,
 	checkedIndicator: `
-		<svg
+		<svg 
 			part="checked-indicator"
 			class="checked-indicator"
-			viewBox="0 0 20 20"
-			xmlns="http://www.w3.org/2000/svg"
+			width="16" 
+			height="16" 
+			viewBox="0 0 16 16" 
+			xmlns="http://www.w3.org/2000/svg" 
+			fill="currentColor"
 		>
-			<path
-				fill-rule="evenodd"
-				clip-rule="evenodd"
-				d="M8.143 12.6697L15.235 4.5L16.8 5.90363L8.23812 15.7667L3.80005 11.2556L5.27591 9.7555L8.143 12.6697Z"
+			<path 
+				fill-rule="evenodd" 
+				clip-rule="evenodd" 
+				d="M14.431 3.323l-8.47 10-.79-.036-3.35-4.77.818-.574 2.978 4.24 8.051-9.506.764.646z"
 			/>
 		</svg>
 	`,
