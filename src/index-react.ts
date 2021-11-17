1e96a311c75b64405f537a41ef0944614a4d0648
Matt Rothenberg <mattrothenberg@github.com>
2021-11-17 15:36:10 -0500

    feat: add script to build react library

--- src/index-react.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {provideReactWrapper} from '@microsoft/fast-react-wrapper';
-import {provideFASTDesignSystem} from '@microsoft/fast-components';
-
-import {vsCodeButton} from '.';
-
-const {wrap} = provideReactWrapper(React, provideFASTDesignSystem());
-
-export const VSCodeButton = wrap(vsCodeButton());
