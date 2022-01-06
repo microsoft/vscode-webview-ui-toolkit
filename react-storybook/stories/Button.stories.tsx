3ca6b0824dda77f8e07328d0c4b2fcbf246d468b
Matt Rothenberg <mattrothenberg@github.com>
2022-01-06 16:57:17 -0500

    feat: get imports working

--- react-storybook/stories/Button.stories.tsx
+++ react-storybook/stories/Button.stories.tsx
@@ -1,42 +1,17 @@
 import React from 'react';
-
-const Button = () => {
-	return <button>Press Me</button>;
-};
+import {VSCodeButton} from '../../src/react';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
 	title: 'Example/Button',
-	component: Button,
-	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+	component: VSCodeButton,
 	argTypes: {
-		backgroundColor: {control: 'color'},
+		// backgroundColor: {control: 'color'},
 	},
 };
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = args => <Button {...args} />;
+const Template = args => <VSCodeButton {...args}>Button Text</VSCodeButton>;
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-	primary: true,
-	label: 'Button',
-};
+export const Default = Template.bind({});
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-	label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-	size: 'large',
-	label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-	size: 'small',
-	label: 'Button',
-};
+Default.args = {};
