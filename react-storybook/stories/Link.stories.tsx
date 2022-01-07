bf82998f01b4661f0c23d5b0f9baa34f451066ec
Matt Rothenberg <mattrothenberg@gmail.com>
2022-01-06 19:06:29 -0500

    feat: add link stories

---
+++ react-storybook/stories/Link.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {VSCodeLink} from '../../src/react';
+
+export default {
+	title: 'Library/Link',
+	component: VSCodeLink,
+	argTypes: {
+		label: {
+			defaultValue: 'vscode-webview-ui-toolkit',
+			control: 'text',
+		},
+		href: {
+			control: 'text',
+			defaultValue:
+				'https://github.com/microsoft/vscode-webview-ui-toolkit',
+		},
+	},
+};
+
+const Template = args => {
+	const {label, ...rest} = args;
+	return (
+		<>
+			<VSCodeLink {...rest}>{label}</VSCodeLink>
+		</>
+	);
+};
+
+export const Default = Template.bind({});
+Default.args = {};
