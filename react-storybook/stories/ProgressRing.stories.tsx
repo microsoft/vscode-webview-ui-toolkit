ecc8df1ba5a7bf6af787926f537a946a9edfed25
Matt Rothenberg <mattrothenberg@gmail.com>
2022-01-07 08:03:11 -0500

    feat: add progress ring

---
+++ react-storybook/stories/ProgressRing.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {VSCodeProgressRing} from '../../src/react';
+
+export default {
+	title: 'Library/Progress Ring',
+	component: VSCodeProgressRing,
+};
+
+const Template = () => {
+	return (
+		<>
+			<VSCodeProgressRing />
+		</>
+	);
+};
+
+export const Default = Template.bind({});
+Default.args = {};
