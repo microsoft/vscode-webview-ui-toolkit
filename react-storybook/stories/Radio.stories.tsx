4a87b6670d91b4a74fdab0e4e871cfab692db49b
Matt Rothenberg <mattrothenberg@github.com>
2022-01-07 10:08:52 -0500

    feat: add radio stories

---
+++ react-storybook/stories/Radio.stories.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import {VSCodeRadio} from '../../src/react';
+
+export default {
+	title: 'Library/Radio',
+	component: VSCodeRadio,
+	argTypes: {
+		onChange: {action: 'change'},
+		label: {
+			defaultValue: 'Option Label',
+			control: 'text',
+		},
+		disabled: {
+			defaultValue: false,
+			control: 'boolean',
+		},
+		checked: {
+			defaultValue: false,
+			control: 'boolean',
+		},
+		readOnly: {
+			defaultValue: false,
+			control: 'boolean',
+		},
+		value: {
+			control: 'text',
+		},
+	},
+};
+
+const Template = args => {
+	const {label, ...rest} = args;
+	return (
+		<>
+			<VSCodeRadio {...rest}>{label}</VSCodeRadio>
+		</>
+	);
+};
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const WithChecked = Template.bind({});
+WithChecked.args = {
+	checked: true,
+};
+
+export const WithDisabled = Template.bind({});
+WithDisabled.args = {
+	disabled: true,
+};
+
+export const WithReadOnly = Template.bind({});
+WithReadOnly.args = {
+	disabled: true,
+};
+
+export const WithValue = Template.bind({});
+WithValue.args = {
+	value: 'baz',
+};
