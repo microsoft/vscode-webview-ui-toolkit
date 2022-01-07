372ee2a526cbbe60793f1d390c664ba8d199d73b
Matt Rothenberg <mattrothenberg@gmail.com>
2022-01-07 08:00:43 -0500

    feat: add tag

---
+++ react-storybook/stories/Tag.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {VSCodeTag} from '../../src/react';
+
+export default {
+	title: 'Library/Tag',
+	component: VSCodeTag,
+	argTypes: {
+		label: {
+			defaultValue: 'Tag Text',
+			control: 'text',
+		},
+	},
+};
+
+const Template = args => {
+	const {label, ...rest} = args;
+	return (
+		<>
+			<VSCodeTag {...rest}>{label}</VSCodeTag>
+		</>
+	);
+};
+
+export const Default = Template.bind({});
+Default.args = {};
