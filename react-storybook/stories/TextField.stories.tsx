e35bd4d80655fc86d12602635f359900b1a20eae
Matt Rothenberg <mattrothenberg@gmail.com>
2022-01-07 09:44:30 -0500

    wip textfield

---
+++ react-storybook/stories/TextField.stories.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+
+import {VSCodeTextField} from '../../src/react';
+
+export default {
+	title: 'Library/Text Field',
+	component: VSCodeTextField,
+	argTypes: {
+		onChange: {action: 'change'},
+		label: {
+			defaultValue: 'Text Field Label',
+			control: 'text',
+		},
+		placeholder: {
+			control: 'text',
+		},
+		disabled: {
+			defaultValue: false,
+			control: 'boolean',
+		},
+		size: {
+			control: 'number',
+		},
+		type: {
+			defaultValue: 'text',
+			control: {
+				type: 'select',
+				options: ['email', 'password', 'tel', 'text', 'url'],
+			},
+		},
+		maxLength: {
+			control: 'number',
+		},
+		readOnly: {
+			defaultValue: false,
+			control: 'boolean',
+		},
+		startIcon: {
+			defaultValue: false,
+			control: 'boolean',
+		},
+		endIcon: {
+			defaultValue: false,
+			control: 'boolean',
+		},
+	},
+};
+
+const Template = args => {
+	const {label, startIcon, endIcon, ...rest} = args;
+	return (
+		<>
+			<VSCodeTextField {...rest}>{label}</VSCodeTextField>
+		</>
+	);
+};
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const WithPlaceholder = Template.bind({});
+WithPlaceholder.args = {
+	placeholder: 'Placeholder goes here',
+};
+
+export const WithDisabled = Template.bind({});
+WithDisabled.args = {
+	disabled: true,
+};
+
+export const WithSize = Template.bind({});
+WithSize.args = {
+	size: 50,
+};
+
+export const WithType = Template.bind({});
+WithType.args = {
+	type: 'password',
+};
+
+export const WithMaxLength = Template.bind({});
+WithMaxLength.args = {
+	maxLength: 10,
+	placeholder: '10 characters max',
+};
+
+export const WithReadOnly = Template.bind({});
+WithReadOnly.args = {
+	readOnly: true,
+};
+
+export const WithStartIcon = Template.bind({});
+WithStartIcon.args = {
+	readOnly: true,
+};
