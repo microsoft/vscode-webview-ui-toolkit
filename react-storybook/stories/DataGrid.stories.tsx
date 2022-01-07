77e924bcff0206e5e766b504137f0387243f1fb6
Matt Rothenberg <mattrothenberg@github.com>
2022-01-07 10:45:43 -0500

    feat: add data grid

--- react-storybook/stories/DataGrid.stories.tsx
+++ react-storybook/stories/DataGrid.stories.tsx
@@ -21,61 +21,63 @@ export default {
 				options: ['default', 'sticky'],
 			},
 		},
+		'numCols': {
+			defaultValue: 4,
+			control: 'number',
+		},
+		'numRows': {
+			defaultValue: 4,
+			control: 'number',
+		},
 		'gridTemplateColumns': {control: 'text'},
 		'aria-label': {defaultValue: 'Basic', control: 'text'},
 	},
 };
 
 const Template = args => {
-	const {generateHeader, gridTemplateColumns, ...rest} = args;
+	const {generateHeader, gridTemplateColumns, numCols, numRows, ...rest} =
+		args;
+
+	const colCells = Array.from({length: numCols}, (_, i) => i);
+	const rowCells = Array.from({length: numRows}, (_, i) => i);
+
 	return (
 		<VSCodeDataGrid
 			gridTemplateColumns={gridTemplateColumns}
 			generateHeader={generateHeader}
 			{...rest}
 		>
-			<VSCodeDataGridRow rowType={DataGridRowTypes.default}>
-				<VSCodeDataGridCell cell-type="columnheader" grid-column="1">
-					Header 1
-				</VSCodeDataGridCell>
-				<VSCodeDataGridCell cell-type="columnheader" grid-column="2">
-					Header 2
-				</VSCodeDataGridCell>
-				<VSCodeDataGridCell cell-type="columnheader" grid-column="3">
-					Header 3
-				</VSCodeDataGridCell>
-				<VSCodeDataGridCell cell-type="columnheader" grid-column="4">
-					Header 4
-				</VSCodeDataGridCell>
+			<VSCodeDataGridRow
+				rowType={
+					generateHeader === 'sticky'
+						? DataGridRowTypes.stickyHeader
+						: DataGridRowTypes.header
+				}
+			>
+				{colCells.map((_, index) => (
+					<VSCodeDataGridCell
+						cell-type="columnheader"
+						gridColumn={`${index + 1}`}
+						key={`${index + 1}`}
+					>
+						Header {index + 1}
+					</VSCodeDataGridCell>
+				))}
 			</VSCodeDataGridRow>
-			<VSCodeDataGridRow>
-				<VSCodeDataGridCell grid-column="1">
-					Cell Data
-				</VSCodeDataGridCell>
-				<VSCodeDataGridCell grid-column="2">
-					Cell Data
-				</VSCodeDataGridCell>
-				<VSCodeDataGridCell grid-column="3">
-					Cell Data
-				</VSCodeDataGridCell>
-				<VSCodeDataGridCell grid-column="4">
-					Cell Data
-				</VSCodeDataGridCell>
-			</VSCodeDataGridRow>
-			<VSCodeDataGridRow>
-				<VSCodeDataGridCell grid-column="1">
-					Cell Data
-				</VSCodeDataGridCell>
-				<VSCodeDataGridCell grid-column="2">
-					Cell Data
-				</VSCodeDataGridCell>
-				<VSCodeDataGridCell grid-column="3">
-					Cell Data
-				</VSCodeDataGridCell>
-				<VSCodeDataGridCell grid-column="4">
-					Cell Data
-				</VSCodeDataGridCell>
-			</VSCodeDataGridRow>
+			{rowCells.map((_, rowIndex) => {
+				return (
+					<VSCodeDataGridRow key={rowIndex}>
+						{colCells.map((_, index) => (
+							<VSCodeDataGridCell
+								key={`${index + 1}`}
+								gridColumn={`${index + 1}`}
+							>
+								Row {rowIndex + 1} Cell {index + 1}
+							</VSCodeDataGridCell>
+						))}
+					</VSCodeDataGridRow>
+				);
+			})}
 		</VSCodeDataGrid>
 	);
 };
