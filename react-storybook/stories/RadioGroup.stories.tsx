5e684c3b3401d8736532b36c5928610c5210801f
Matt Rothenberg <mattrothenberg@github.com>
2022-01-07 10:13:54 -0500

    feat: add radio group stories

---
+++ react-storybook/stories/RadioGroup.stories.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import {VSCodeRadio, VSCodeRadioGroup} from '../../src/react';
+
+export default {
+	title: 'Library/Radio Group',
+	component: VSCodeRadioGroup,
+	argTypes: {
+		label: {defaultValue: 'Radio Group', control: 'text'},
+		orientation: {
+			defaultValue: 'horizontal',
+			control: {
+				type: 'select',
+				options: ['vertical', 'horizontal'],
+			},
+		},
+		numberOfChildren: {defaultValue: 3, control: 'number'},
+		disabled: {control: 'boolean'},
+		readOnly: {control: 'boolean'},
+	},
+};
+
+const Template = args => {
+	const {label, numberOfChildren, ...rest} = args;
+
+	const options = Array.from({length: numberOfChildren}, (_, i) => i);
+
+	return (
+		<>
+			<VSCodeRadioGroup {...rest}>
+				<label slot="label">{label}</label>
+				{options.map((_, index) => (
+					<VSCodeRadio value={`Option #${index + 1}`}>
+						{`Option #${index + 1}`}
+					</VSCodeRadio>
+				))}
+			</VSCodeRadioGroup>
+		</>
+	);
+};
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const WithVerticalOrientation = Template.bind({});
+WithVerticalOrientation.args = {
+	orientation: 'vertical',
+};
+
+export const WithDisabled = Template.bind({});
+WithDisabled.args = {
+	disabled: true,
+};
+
+export const WithReadOnly = Template.bind({});
+WithReadOnly.args = {
+	readOnly: true,
+};
