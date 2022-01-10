148c9c8badd9677fd39f696c28003183405af0e9
Matt Rothenberg <mattrothenberg@github.com>
2022-01-10 17:33:25 -0500

    feat: add a11y plugin

--- react-storybook/.storybook/main.js
+++ react-storybook/.storybook/main.js
@@ -3,7 +3,11 @@ module.exports = {
 		'../stories/**/*.stories.mdx',
 		'../stories/**/*.stories.@(js|jsx|ts|tsx)',
 	],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-a11y',
+	],
 	framework: '@storybook/react',
 	features: {
 		postcss: false,
