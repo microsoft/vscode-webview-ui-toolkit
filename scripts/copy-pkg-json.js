c85751d0a09eb6d2ff2226b445713276993c2529
Matt Rothenberg <mattrothenberg@github.com>
2022-01-24 14:18:00 -0500

    feat: wrap copy script in try/catch

--- scripts/copy-pkg-json.js
+++ scripts/copy-pkg-json.js
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
 const fs = require('fs');
 
-// get the package.json file
-const pkg = JSON.parse(fs.readFileSync('package.json'));
+async function main() {
+	try {
+		// get the package.json file
+		const pkg = JSON.parse(fs.readFileSync('package.json'));
 
-// modify the "main" and "types" fields of the package.json file
-pkg.main = 'esm/index.js';
-pkg.types = 'dts/index.d.ts';
+		// modify the "main" and "types" fields of the package.json file
+		pkg.main = 'esm/index.js';
+		pkg.types = 'dts/index.d.ts';
 
-// write the new package file to dist
-fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, 2));
+		// write the new package file to dist
+		fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, 2));
+	} catch (err) {
+		console.log(
+			`${color(
+				['red'],
+				'Error: Reading or copying package.json file failed.'
+			)}\n    ${err}`
+		);
+		process.exit();
+	}
+}
+
+main();
