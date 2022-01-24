5e284e3c17d0b5d14aab5390d82e5fb6390ce53b
Matt Rothenberg <mattrothenberg@github.com>
2022-01-24 14:21:54 -0500

    fix: add separate try/catch blocks

--- scripts/copy-pkg-json.js
+++ scripts/copy-pkg-json.js
@@ -2,10 +2,16 @@
 const fs = require('fs');
 
 async function main() {
-	try {
-		// get the package.json file
-		const pkg = JSON.parse(fs.readFileSync('package.json'));
+	let pkg = {};
 
+	try {
+		pkg = JSON.parse(fs.readFileSync('package.json'));
+	} catch (err) {
+		console.log(`Error: Parsing package.json file failed.\n    ${err}`);
+		process.exit();
+	}
+
+	try {
 		// modify the "main" and "types" fields of the package.json file
 		pkg.main = 'esm/index.js';
 		pkg.types = 'dts/index.d.ts';
@@ -14,10 +20,7 @@ async function main() {
 		fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, 2));
 	} catch (err) {
 		console.log(
-			`${color(
-				['red'],
-				'Error: Reading or copying package.json file failed.'
-			)}\n    ${err}`
+			`Error: Writing modified package.json file failed.\n    ${err}`
 		);
 		process.exit();
 	}
