df516f912d41cc4120debdeaec3734861c1fc52e
Hawk Ticehurst <hawkticehurst@gmail.com>
2022-01-24 17:52:07 -0800

    Update setup webview test env script to use helper functions and copy over new react build dir

--- scripts/setup-webview-test-env.js
+++ scripts/setup-webview-test-env.js
@@ -1,12 +1,10 @@
-#!/usr/bin/env node
-
+const {createDir, copyDir, color, delDir} = require('./helpers');
 const {exec} = require('child_process');
 const fs = require('fs');
-const path = require('path');
 const process = require('process');
 const util = require('util');
 
-const execPromise = util.promisify(exec);
+const execShellCommand = util.promisify(exec);
 
 async function main() {
 	// Empty print line to pretty-ify command line output
@@ -35,10 +33,20 @@ async function main() {
 	}
 
 	// Copy latest toolkit build into the webview test environment
-	console.log(color(['dim'], 'Copying latest toolkit build into webview test environment...'));
-	delDir('./test-webview/node_modules/@vscode/webview-ui-toolkit/dist');
-	createDir('./test-webview/node_modules/@vscode/webview-ui-toolkit/dist');
-	copyDir('./dist', './test-webview/node_modules/@vscode/webview-ui-toolkit');
+	try {
+		console.log(color(['dim'], 'Copying latest toolkit build into webview test environment...'));
+		// Copy web component build directory
+		delDir('./test-webview/node_modules/@vscode/webview-ui-toolkit/dist');
+		createDir('./test-webview/node_modules/@vscode/webview-ui-toolkit/dist');
+		copyDir('./dist', './test-webview/node_modules/@vscode/webview-ui-toolkit');
+		// Copy react build directory
+		delDir('./test-webview/node_modules/@vscode/webview-ui-toolkit/react');
+		createDir('./test-webview/node_modules/@vscode/webview-ui-toolkit/react');
+		copyDir('./react', './test-webview/node_modules/@vscode/webview-ui-toolkit');
+	} catch (err) {
+		console.log(`${color(['red'], 'Error: Failed to copy latest toolkit build into webview test environment')}\n    ${err}`);
+		process.exit();
+	}
 
 	// Print success and next steps messages
 	console.log();
@@ -49,76 +57,6 @@ async function main() {
 	console.log(`  2. Press ${color(['cyan'], 'F5')} to open the webview test extension with the most recent toolkit build loaded`);
 	console.log(`  3. Run the "${color(['cyan'], 'Webview UI Toolkit: All Components')}" command using the VS Code command palette`);
 	console.log();
-}
-
-async function execShellCommand(command) {
-	return await execPromise(command);
-}
-
-function delDir(path) {
-	if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
-		fs.readdirSync(path).forEach(function (file, index) {
-			const currPath = path + '/' + file;
-			if (fs.lstatSync(currPath).isDirectory()) {
-				delDir(currPath);
-			} else {
-				fs.unlinkSync(currPath);
-			}
-		});
-		fs.rmdirSync(path);
-	}
-}
-
-function createDir(dir) {
-	if (!fs.existsSync(dir)) {
-		fs.mkdirSync(dir);
-	}
-}
-
-function copyDir(source, target) {
-	let files = [];
-	const targetFolder = path.join(target, path.basename(source));
-	if (!fs.existsSync(targetFolder)) {
-		fs.mkdirSync(targetFolder);
-	}
-	if (fs.lstatSync(source).isDirectory()) {
-		files = fs.readdirSync(source);
-		files.forEach(function (file) {
-			const curSource = path.join(source, file);
-			if (fs.lstatSync(curSource).isDirectory()) {
-				copyDir(curSource, targetFolder);
-			} else {
-				copyFileSync(curSource, targetFolder);
-			}
-		});
-	}
-}
-
-function copyFileSync(source, target) {
-	let targetFile = target;
-	if (fs.existsSync(target)) {
-		if (fs.lstatSync(target).isDirectory()) {
-			targetFile = path.join(target, path.basename(source));
-		}
-	}
-	fs.writeFileSync(targetFile, fs.readFileSync(source));
-}
-
-const colors = {
-	reset: '\x1b[0m',
-	bold: '\x1b[1m',
-	dim: '\x1b[2m',
-	red: '\x1b[31m',
-	green: '\x1b[32m',
-	cyan: '\x1b[36m',
-};
-
-function color(opts, text) {
-	let colorString = '';
-	for (const opt of opts) {
-		colorString += colors[opt];
-	}
-	return `${colorString}${text}${colors.reset}`;
 }
 
 main();
