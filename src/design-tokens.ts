d88ada0f6b1f6fd342f872de4ea5de22e4c8e5fb
Hawk Ticehurst <hawkticehurst@gmail.com>
2022-02-02 15:41:20 -0800

    Create a new design token to help with fixing the icon button high contrast theme bug

--- src/design-tokens.ts
+++ src/design-tokens.ts
@@ -4,6 +4,23 @@
 import {create} from './utilities/design-tokens/create';
 
 /**
+ * A note about `--fake-vscode-token` usage:
+ *
+ * There are some tokens defined in this file that make use of `--fake-vscode-token`. This is
+ * done when a toolkit token should be added to the tokenMappings map (and subsequently altered
+ * in the applyTheme function) but does not have a corresponding VS Code token that can be used.
+ *
+ * An example is buttonIconHoverBackground token which does not have a corresponding VS Code token
+ * at this time (it's a hardcoded value in VS Code), but needs to be adjusted to transparent when a
+ * high contrast theme is applied.
+ *
+ * As a rule of thumb, if there are special cases where a token needs to be adjusted based on the
+ * VS Code theme and does not have a corresponding VS Code token, the `--fake-vscode-token` can be used
+ * to indicate that it should be added to the tokenMappings map and thus make it accessible to the
+ * applyTheme function where it can be adjusted.
+ */
+
+/**
  * Global design tokens.
  */
 
@@ -47,7 +64,8 @@ export const buttonBorder = create<string>('button-border', '--vscode-button-bor
 export const buttonIconBackground = create<string>('button-icon-background').withDefault('transparent');
 export const buttonIconCornerRadius = create<string>('button-icon-corner-radius').withDefault('5px');
 export const buttonIconFocusBorderOffset = create<number>('button-icon-outline-offset').withDefault(0);
-export const buttonIconHoverBackground = create<string>('button-icon-hover-background').withDefault('rgba(90, 93, 94, 0.31)');
+// Note usage of `--fake-vscode-token` (refer to doc comment at top of file for explanation).
+export const buttonIconHoverBackground = create<string>('button-icon-hover-background', '--fake-vscode-token').withDefault('rgba(90, 93, 94, 0.31)');
 export const buttonIconPadding = create<string>('button-icon-padding').withDefault('3px');
 export const buttonPrimaryBackground = create<string>('button-primary-background', '--vscode-button-background').withDefault('#0e639c');
 export const buttonPrimaryForeground = create<string>('button-primary-foreground', '--vscode-button-foreground').withDefault('#ffffff');
