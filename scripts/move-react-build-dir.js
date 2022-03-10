4153c6d28ca6959d7489bb2bcd5a669a8afa1fcc
Hawk Ticehurst <hawkticehurst@gmail.com>
2022-03-09 16:54:26 -0800

    Add copyright notice to various source code files

--- scripts/move-react-build-dir.js
+++ scripts/move-react-build-dir.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 const {copyDir, color, delDir} = require('./helpers');
 const fsPromises = require('fs').promises;
 const process = require('process');
--- src/react/index.ts
+++ src/react/index.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import {provideReactWrapper} from '@microsoft/fast-react-wrapper';
 import React from 'react';
 import {
