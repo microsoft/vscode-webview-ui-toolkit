b3f7c4f42b0c85946b60c41a02da9907dae54119
Hawk Ticehurst <hawkticehurst@gmail.com>
2023-12-05 10:39:43 -0800

    Remove deprecated eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-non-null-assertion': 'off',
-		'@typescript-eslint/interface-name-prefix': 'off',
 	},
 	overrides: [
 		{
